9e973fa2284ed09467869f1d29685e85a8429dfb
Lindsey Kuper <lindsey@composition.al>
2013-03-06 23:58:03 -0500

    Starting to work on graph traversal.  Doesn't typecheck yet...

--- test-level-synchronized-bfs.hs
+++ test-level-synchronized-bfs.hs
@@ -1,11 +1,12 @@
-import LVarTraceInternal
+{-# LANGUAGE FlexibleContexts #-}
+
+import LVarTraceInternal (newEmptySet, putInSet, Par, runParIO, ISet)
+import Control.Monad.Par.Combinator (parMap)
+import Control.Monad.Par.Class (ParFuture)
 import Data.Maybe (fromJust)
 import qualified Data.Set as Set
-
---------------------------------------------------------------------------------
-
--- | OK, so I want to do a breadth-first search of a graph...I need a
--- graph to search, then!
+import Control.DeepSeq (NFData)
+import Data.Traversable (Traversable)
 
 -- Graph representation
 data Node a = Node
@@ -60,5 +61,44 @@ graphExample =
 printGraph g =
   putStrLn . filter (`notElem` "'\"") . show $ g
   
+{-
+
+From Ryan's intro draft: "In a graph, find the connected component
+containing a vertex V, and compute a function F over the labels of all
+vertices in that component, returning a set of results."
+
+Nothing about that problem statement says it has to be a breadth-first
+search.  Should it?  (Also, this isn't really a "search", just a
+breadth-first traversal.)
+
+-}
+
 main =
-  printGraph graphExample
+  runParIO $ do
+    let g = graphExample
+    let v = 'a'
+    let f = \x -> x
+    l_acc <- newEmptySet
+    result <- bf_traverse g l_acc Set.empty (Set.singleton v) f
+    return result
+
+-- Takes a graph, an LVar, a set of "seen" node labels, a set of "new"
+-- node labels, and the function f to be applied to each node.  We're
+-- not actually doing anything with f yet.
+bf_traverse :: (Graph a) -> ISet a ->
+                  Set.Set a -> Set.Set a -> (a -> b) -> Par (Set.Set b)
+bf_traverse g l_acc seen_rank new_rank f =
+  if Set.null new_rank
+  then return Set.empty
+  else do
+    let seen_rank' = Set.union seen_rank new_rank
+    -- Add to the next rank, and to the output/accumulator:
+    let add n = if Set.member n seen_rank'
+                then return Set.empty
+                else do putInSet n l_acc
+                        return (Set.singleton n)
+    let new_rank' =
+          foldr Set.union Set.empty (parMap (parMap add . (nbrLabels g))
+                                     new_rank)
+    bf_traverse g l_acc seen_rank' new_rank' f
+
