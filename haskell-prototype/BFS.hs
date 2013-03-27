c7d6065e3161c95b1562e9bb0903c12205b1efe9
Lindsey Kuper <lindsey@composition.al>
2013-03-26 20:17:01 -0400

    Throw in lots of strictness

--- haskell-prototype/BFS.hs
+++ haskell-prototype/BFS.hs
@@ -4,9 +4,9 @@
 
 #ifdef PURE
 #warning "Using the PURE version"
-import LVarTracePure (newEmptySet, putInSet, Par, runParIO, ISet, consumeSet, fork)
+import LVarTracePure (newEmptySet, putInSet, Par, runParIO, ISet, consumeSet, fork, liftIO)
 #else
-import LVarTraceInternal (newEmptySet, putInSet, Par, runParIO, ISet, consumeSet, fork)
+import LVarTraceInternal (newEmptySet, putInSet, Par, runParIO, ISet, consumeSet, fork, liftIO)
 #endif
 
 import GHC.Conc (numCapabilities)
@@ -54,7 +54,9 @@ mkGraphFromFile = do
   let tuplify2 [x,y] = (x, y)
   -- Ignore the initial "EdgeArray" string in the pbbs-generated file.
   let (_:stringpairs) = map tuplify2 (map (splitOn " ") (lines inStr))
-  return (mkGraph (map (\(x,y) -> (read x::Int, read y::Int)) stringpairs))
+  -- return (mkGraph (map (\(x,y) -> (read x::Int, read y::Int)) stringpairs))
+  g <- evaluate (mkGraph (map (\(x,y) -> (read x::Int, read y::Int)) stringpairs))
+  return g
   
 -- Neighbors of a node with a given label
 nbrs :: Graph -> Int -> [Int]
@@ -66,11 +68,11 @@ printGraph g = do
   putStrLn (show ls)
   return ()
     
--- Iterates the sin function x times on its input.  This takes a few
--- seconds for, e.g., n = 10,000,000.
+-- Iterates the sin function n times on its input and returns the sum
+-- of all iterations.
 sin_iter :: Int -> Float -> Float
 sin_iter 0  x = x
-sin_iter n !x = sin_iter (n-1) (x + sin x)
+sin_iter n !x = sin_iter (n - 1) (x + sin x)
 
 myConfig :: Config
 myConfig = defaultConfig {
@@ -85,13 +87,11 @@ main :: IO ()
 main = do
   g <- mkGraphFromFile
   let startNode = 0
-      
-  -- We have to pick a particular Floating type for the result of
-  -- sin_iter, or the typechecker will get confused.
+  
   let graphThunk :: (Float -> Float) -> IO ()
       graphThunk fn = do st <- start_traverse g 0 fn
-                         putStrLn "Done with start_traverse, going to pop the final thunk."                         
-                         evaluate st                         
+                         putStrLn "Done with start_traverse, going to pop the final thunk."
+                         evaluate st
                          putStrLn "Done!"
 --  let sin_iter_count = sin_iter 10000000
   let sin_iter_count = sin_iter 10
@@ -101,7 +101,7 @@ main = do
 #else
   defaultMainWith myConfig (return ()) [
          bgroup "bf_traverse" [
-           bench "sin_iter_ten_million" $ graphThunk sin_iter_count
+           bench "sin_iter" $ graphThunk sin_iter_count
          ]
          ]
 #endif
@@ -109,15 +109,18 @@ main = do
 -- Takes a graph, a start node, and a function to be applied to each
 -- node.
 start_traverse :: Graph -> Int -> (Float -> Float) -> IO (Set.Set Float)
-start_traverse g startNode f = do
+start_traverse !g startNode f = do
   runParIO $ do
         prnt $ "Running on " ++ show numCapabilities ++ " parallel resources..."
         l_acc <- newEmptySet
         -- "manually" add startNode
         putInSet (f (fromIntegral startNode)) l_acc
         result <- bf_traverse 0 g l_acc Set.empty (Set.singleton startNode) f
+        prnt $ "Evaling result..."
+        liftIO (do evaluate result; return ())
         prnt $ "Done with bf_traverse... "
         s <- consumeSet l_acc :: Par (Set.Set Float)
+        liftIO (do evaluate s; return ()) -- this explodes
         prnt $ "Done consume set ... "
         return s
 
@@ -125,8 +128,8 @@ start_traverse g startNode f = do
 -- node labels, and the function f to be applied to each node.
 bf_traverse :: Int -> Graph -> ISet Float -> Set.Set Int -> Set.Set Int -> (Float -> Float)
                -> Par (Set.Set Float)
-bf_traverse n g l_acc !seen_rank !new_rank f = do 
-  trace ("Bf_traverse call.. "++show n++" seen/new size "
+bf_traverse n !g !l_acc !seen_rank !new_rank !f = do 
+  trace ("bf_traverse call.. "++show n++" seen/new size "
          ++show (Set.size seen_rank, Set.size new_rank)) $ return () 
   
   -- Nothing in the new_rank set means nothing left to traverse.
