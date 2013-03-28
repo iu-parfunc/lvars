5751c15b17b735ed9802312aee576b72a48e2687
Ryan Newton <rrnewton@gmail.com>
2013-03-28 19:42:01 -0400

    Put in infrastructure for busywaiting

--- haskell-prototype/Runner.hs
+++ haskell-prototype/Runner.hs
@@ -22,7 +22,11 @@ import           Control.Monad (forM_, when)
 import           Control.Monad.Par.Combinator (parMap, parMapM, parFor, InclusiveRange(..))
 import           Control.Monad.Par.Class (ParFuture)
 import           Control.DeepSeq         (NFData)
+import           Data.Int
+import           Data.Word
+import           Data.IORef
 import           Data.List as L
+import           Data.List.Split (chunksOf)
 import qualified Data.Set as Set
 import qualified Data.IntSet as IS
 import qualified Data.ByteString.Lazy.Char8 as B
@@ -34,7 +38,9 @@ import           Text.Printf (printf)
 import           System.Mem (performGC)
 import           System.IO.Unsafe (unsafePerformIO)
 import           System.Environment (getEnvironment,getArgs)
-
+import           System.CPUTime.Rdtsc (rdtsc)
+-- import           Data.Time.Clock (getCurrentTime)
+import           System.CPUTime  (getCPUTime)
 import qualified Control.Parallel.Strategies as Strat
 
 -- For parsing the file produced by pbbs
@@ -52,8 +58,10 @@ import qualified Data.Vector.Mutable as MV
 
 -- Vector representation of graphs: the list (or set) at index k is
 -- node k's neighbors.
-type Graph = V.Vector [Int]
+type Graph = V.Vector [Node]
 type Graph2 = V.Vector IS.IntSet
+
+type Node = Int
 
 -- Optimized version:
 mkGraphFromFile :: String -> IO Graph
@@ -99,12 +107,12 @@ printGraph g = do
     
 -- Iterates the sin function n times on its input and returns the sum
 -- of all iterations.
-sin_iter :: Int -> Float -> Float
+sin_iter :: Word64 -> Float -> Float
 sin_iter 0  x = x
 sin_iter n !x = sin_iter (n - 1) (x + sin x)
 
-type WorkRet = (Float, Int)
-type WorkFn = (Int -> WorkRet)
+type WorkRet = (Float, Node)
+type WorkFn = (Node -> WorkRet)
 
 prnt :: String -> Par ()
 prnt str = trace str $ return ()
@@ -136,7 +144,7 @@ main = do
   let k_ :: Int
       k_ = 25    -- Number of hops to explore
       
-  let w_ :: Int
+  let w_ :: Word64
       w_ = 20000 -- Amount of work (iterations of sin)
 
   args <- getArgs
@@ -147,8 +155,8 @@ main = do
         case args of
           []                   -> (graphFile_, k_, w_)
           [graphFiles]         -> (graphFiles, k_, w_)
-          [graphFiles, ks]     -> (graphFiles, read ks :: Int, w_)
-          [graphFiles, ks, ws] -> (graphFiles, read ks :: Int, read ws :: Int)
+          [graphFiles, ks]     -> (graphFiles, read ks, w_)
+          [graphFiles, ks, ws] -> (graphFiles, read ks, read ws :: Word64)
   
   g <- mkGraphFromFile graphFile
 
@@ -165,15 +173,63 @@ main = do
       sin_iter_count x = let f = fromIntegral x in
                          (sin_iter w f, x)
 
+  freq <- measureFreq
+  printf "CPU Frequency: %s\n" (commaint freq)  
   printf "* Beginning benchmark with k=%d and w=%d\n" k w
+  let busy_waiter :: WorkFn
+      busy_waiter n = unsafePerformIO $
+        wait_microsecs freq w n
 
   performGC
   t0 <- getCurrentTime
-  graphThunk sin_iter_count
+--  graphThunk sin_iter_count
+  graphThunk busy_waiter  
   t1 <- getCurrentTime
   putStrLn $ "SELFTIMED " ++ show (diffUTCTime t1 t0) ++ "\n"
 
 ------------------------------------------------------------------------------------------
 
+first_hit :: IORef Int64
+first_hit = unsafePerformIO$ newIORef maxBound
 
+-- Wait for a certain number of milleseconds.
+wait_microsecs :: Word64 -> Word64 -> Node -> IO WorkRet
+wait_microsecs clockFreq micros n =
+  return (3.3, n)
+
+-- Measure clock frequency, spinning rather than sleeping to try to
+-- stay on the same core.
+measureFreq :: IO Word64
+measureFreq = do
+  let millisecond = 1000 * 1000 * 1000 -- picoseconds are annoying
+      -- Measure for how long to be sure?      
+      measure = 200 * millisecond
+--      measure = 1000 * millisecond      
+      scale :: Integer
+      scale = 1000 * millisecond `quot` measure
+  t1 <- rdtsc 
+  start <- getCPUTime
+  let loop :: Word64 -> Word64 -> IO (Word64,Word64)
+      loop !n !last = 
+       do t2 <- rdtsc 
+	  when (t2 < last) $
+	       putStrLn$ "COUNTERS WRAPPED "++ show (last,t2) 
+	  cput <- getCPUTime		
+	  if (cput - start < measure)
+	   then loop (n+1) t2
+	   else return (n,t2)
+  (n,t2) <- loop 0 t1
+  putStrLn$ "  Approx getCPUTime calls per second: "++ commaint (scale * fromIntegral n)
+  when (t2 < t1) $ 
+    putStrLn$ "WARNING: rdtsc not monotonically increasing, first "++show t1++" then "++show t2++" on the same OS thread"
+
+  return$ fromIntegral (fromIntegral scale * (t2 - t1))
+
+-- Readable large integer printing:
+commaint :: (Show a, Integral a) => a -> String
+commaint n = 
+   reverse $ concat $
+   intersperse "," $ 
+   chunksOf 3 $ 
+   reverse (show n)
 
