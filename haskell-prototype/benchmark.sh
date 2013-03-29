f790deb09884e61b28a56aab0c2e238d94683c2d
Lindsey Kuper <lindsey@composition.al>
2013-03-28 20:41:32 -0400

    Tweak benchmark.sh.
    All it does now is set off benchmark.run with the appropriate env vars.

--- haskell-prototype/benchmark.sh
+++ haskell-prototype/benchmark.sh
@@ -4,36 +4,24 @@ set -e
 
 make data
 
-num_cores="1 2 4";
-# trials=100
-trials=9
-
 if [ $(hostname) = "hive.soic.indiana.edu" ];
 then
     lock_for_experiments 1000
-    num_cores="1 2 4 8 16 32";
+    export THREADS="1 2 4 8 16 32"
+    export TRIALS=9
 fi
 
-## BFS:
+if [ $(hostname) = "lenny" ]; # Lindsey's laptop
+then
+    export THREADS="1 2 4"
+    export TRIALS=3
+fi
 
-# Impure version (experimental -- not working at the moment)
-# make q
-# for i in $num_cores; do ./quick_io.exe +RTS -N$i; done
-
-# Pure version
-make qp
-
-# Run 100 times for each core count.
-
-# Grid graph.  Probably less realistic than a random graph.
-#for i in $num_cores; do ./ntimes_minmedmax 100 ./quick_pure.exe +RTS -N$i; done
-
-# Random graph.
-for i in $num_cores; do 
-  ./ntimes_minmedmax $trials ./quick_lvarpure.exe /tmp/rand +RTS -s -N$i; 
-done
+make benchmark.run
+./benchmark.run
 
 if [ $(hostname) = "hive.soic.indiana.edu" ];
 then
     unlock_for_experiments
 fi
+
