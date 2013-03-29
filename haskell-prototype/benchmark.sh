09cee3f9e09a78ed58941b90f95301a6cde1d8c1
Lindsey Kuper <lindsey@composition.al>
2013-03-28 21:02:20 -0400

    Let's not go overboard with the # of trials.

--- haskell-prototype/benchmark.sh
+++ haskell-prototype/benchmark.sh
@@ -8,13 +8,13 @@ if [ $(hostname) = "hive.soic.indiana.edu" ];
 then
     lock_for_experiments 1000
     export THREADS="1 2 4 8 16 32"
-    export TRIALS=9
+    export TRIALS=5
 fi
 
 if [ $(hostname) = "lenny" ]; # Lindsey's laptop
 then
     export THREADS="1 2 4"
-    export TRIALS=3
+    export TRIALS=1
 fi
 
 make benchmark.run
