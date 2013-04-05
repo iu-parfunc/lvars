26ee7fca02a4734e2a9e87f5bdbbec159c002d9a
Lindsey Kuper <lindsey@composition.al>
2013-04-05 14:58:28 -0400

    Some hints for natpairs.

--- redex/natpair/natpair.rkt
+++ redex/natpair/natpair.rkt
@@ -10,3 +10,13 @@
   ;; FIXME: lub operation goes here
   max
   )
+
+;; Write a function that takes two pairs (they might be of the form
+;; (natural natural), (natural Bot), (Bot natural), or maybe (Bot
+;; Bot)) and returns a pair that is their least upper bound.
+
+;; use car and cadr.  you might have to write a helper
+
+(define my-lub
+  (lambda (p1 p2)
+    ... ))
