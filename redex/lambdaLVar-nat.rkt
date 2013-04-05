7ee228183a3408d5f4d24504eef3dbac6b4d7a8c
Lindsey Kuper <lindsey@composition.al>
2013-04-05 12:51:49 -0400

    First stab at macro-generated Redex.

---
+++ redex/lambdaLVar-nat.rkt
@@ -0,0 +1,9 @@
+;; A Redex model of the lambdaLVar language.  Mentions of "the TR" in
+;; this document refer to
+;; http://www.cs.indiana.edu/cgi-bin/techreports/TRNNN.cgi?trnum=TR702
+
+#lang racket
+(require redex/reduction-semantics)
+(require "lambdaLVar.rkt")
+
+(define-lambdaLVar-language lambdaLVar-nat natural max)
