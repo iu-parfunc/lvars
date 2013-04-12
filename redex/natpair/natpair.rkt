e85cec360db3f09f53a43ed90fe462d60e8ced27
iweating <iweating@indiana.edu>
2013-04-12 14:54:06 -0400

    Defined mylub to determine the lub

--- redex/natpair/natpair.rkt
+++ redex/natpair/natpair.rkt
@@ -8,7 +8,7 @@
   (natural Bot)
   (Bot natural))
 
-;; Write a function that takes two pairs (they might be of the formg
+;; Write a function that takes two pairs (they might be of the form
 ;; (natural natural), (natural Bot), (Bot natural), or maybe (Bot
 ;; Bot)) and returns a pair that is their least upper bound (using max)
 
@@ -17,4 +17,23 @@
 (define my-lub
   (lambda (p1 p2)
     ;; FIXME: define me properly!
-    p1))
+    (let* ([x_1 (car p1)]
+          [x_2 (cadr p1)]
+          [y_1 (car p2)]
+          [y_2 (cadr p2)]
+          [max_1 (mymax x_1 y_1)]
+          [max_2 (mymax x_2 y_2)])
+      (list max_1 max_2))))
+
+(define mymax 
+  (lambda (z_1 z_2)
+    (cond 
+      [(and (number? z_1) (number? z_2))
+       (max z_1 z_2)]
+      [(number? z_1)
+       z_1]
+      [(number? z_2)
+       z_2]
+      [else (error "You didn't pass in a valid argument")])))
+          
+    
