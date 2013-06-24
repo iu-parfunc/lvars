300a7aba66942c0450e26409fa940d66e434feca
Lindsey Kuper <lindsey@composition.al>
2013-06-23 20:08:54 -0400

    Fixed up downset-op for natpair-ivars; got more tests running.

--- redex/LVish/natpair-ivars.rkt
+++ redex/LVish/natpair-ivars.rkt
@@ -17,30 +17,34 @@
   ;; downset-op: Takes a pair p and returns a list of everything below
   ;; or equal to p in the lattice.
 
-  ;; (downset-op (2 . 1)) =>
-  ;; '((Bot . Bot)
-  ;;   (Bot . 0)
-  ;;   (Bot . 1)
-  ;;   (0 . Bot)
-  ;;   (0 . 0)
-  ;;   (0 . 1)
-  ;;   (1 . Bot)
-  ;;   (1 . 0)
-  ;;   (1 . 1)
-  ;;   (2 . Bot)
-  ;;   (2 . 0)
-  ;;   (2 . 1))
+  ;; (downset-op '(2 1)) =>
+  ;; '(Bot
+  ;;   (Bot 0)
+  ;;   (Bot 1)
+  ;;   (0 Bot)
+  ;;   (0 0)
+  ;;   (0 1)
+  ;;   (1 Bot)
+  ;;   (1 0)
+  ;;   (1 1)
+  ;;   (2 Bot)
+  ;;   (2 0)
+  ;;   (2 1))
+  
   (define downset-op
     (lambda (p)
-      (let ([car-iota (append '(Bot) (iota (car p) `(,(car p))))]
-            [cdr-iota (append '(Bot) (iota (cdr p) `(,(cdr p))))])
+      (let ([car-iota (append '(Bot) (iota (car p)) `(,(car p)))]
+            [cadr-iota (append '(Bot) (iota (cadr p)) `(,(cadr p)))])
         ;; car-iota: '(Bot 0 1 2)
-        ;; cdr-iota: '(Bot 0 1)
-        (map (lambda (x)
-               (map (lambda (y)
-                      (cons x y))
-                    cdr-iota))
-             car-iota))))
+        ;; cadr-iota: '(Bot 0 1)
+        (apply append
+               (map (lambda (x)
+                      (map (lambda (y)
+                             (if (and (equal? x 'Bot) (equal? y 'Bot))
+                                 'Bot
+                                 `(,x ,y)))
+                           cadr-iota))
+                    car-iota)))))
 
   ;; my-lub: A function that takes two pairs (they might be of the
   ;; form (natural natural), (natural Bot), or (Bot natural)) and
@@ -144,14 +148,117 @@
      (term (lookup-frozenness ((l ((2 3) #t))) l))
      (term #t))
 
-    ;; FIXME: write more tests
+    (test-equal
+     (term (extend-Df () (3 3)))
+     (term ((3 3))))
+
+    (test-equal
+     (term (extend-Df ((3 3) (4 4) (5 5)) (6 6)))
+     (term ((6 6) (3 3) (4 4) (5 5))))
+
+    (test-equal
+     (term (contains-all-leq (1 1) (Bot (Bot 0) (Bot 1)
+                                    (0 Bot) (0 0) (0 1)
+                                    (1 Bot) (1 0) (1 1))))
+     (term #t))
+
+    (test-equal
+     (term (contains-all-leq (1 1) ((Bot 0) (Bot 1)
+                                    (0 Bot) (0 0) (0 1)
+                                    (1 Bot) (1 0) (1 1))))
+     (term #f))
+
+    (test-equal
+     (term (contains-all-leq (1 1) (Bot (Bot 0) (Bot 1)
+                                    (0 Bot) (0 0) 
+                                    (1 Bot) (1 0) (1 1))))
+     (term #f))
+
+    (test-equal
+     (term (contains-all-leq (1 1) (Bot (Bot 0) (Bot 1)
+                                    (0 Bot) (0 0) 
+                                    (1 Bot) (1 0) (1 1)
+                                    (2 Bot) (2 0) (2 1))))
+     (term #f))
+
+    (test-equal
+     (term (contains-all-leq (1 1) (Bot (Bot 0) (Bot 1)
+                                    (0 Bot) (0 0) (0 1)
+                                    (1 Bot) (1 0) (1 1)
+                                    (2 Bot) (2 0) (2 1))))
+     (term #t))
+
+    ;; For the next few tests, note that (downset (1 1)) =>
+    ;; (Bot (Bot 0) (Bot 1)
+    ;;  (0 Bot) (0 0) (0 1)
+    ;;  (1 Bot (1 0) (1 1)))
+
+    (test-equal
+     (term (first-unhandled-d (1 1) ((Bot 0) (Bot 1))))
+     (term Bot))
+    
+    (test-equal
+     (term (first-unhandled-d (1 1) (Bot (Bot 1))))
+     (term (Bot 0)))
+
+    (test-equal
+     (term (first-unhandled-d (1 1)
+                              (Bot (Bot 0) (Bot 1)
+                               (0 Bot) (0 0) (0 1)
+                               (1 Bot) (1 0) (1 1)
+                               (5 5) (6 6) (7 7))))
+     (term #f))
+
+    (test-equal
+     (term (first-unhandled-d (1 1)
+                              (Bot (Bot 0) (Bot 1)
+                               (0 Bot) (0 0) (0 1)
+                               (1 Bot) (1 0) (1 1))))
+     (term #f))
+
+    (test-equal
+     (term (first-unhandled-d (1 1)
+                              (Bot (Bot 0) (Bot 1)
+                               (0 Bot)
+                               (1 Bot) (1 0) (1 1))))
+     (term (0 0)))
+
+    (test-equal
+     (term (first-unhandled-d (1 1)
+                              (Bot (Bot 0) (Bot 1)
+                               (0 Bot) (0 0) (0 1)
+                               (1 Bot) (1 0))))
+
+     (term (1 1)))
+
+    (test-equal
+     (term (first-unhandled-d (1 1)
+                              (Bot (Bot 0) (Bot 1)
+                               (0 Bot) (0 0) (0 1)
+                               (1 Bot) (1 0)
+                               (5 5) (6 6) (7 7))))
+     (term (1 1)))
+
+    (test-equal
+     (term (first-unhandled-d (1 1)
+                              ((1 Bot) (0 0) (7 7)
+                               (5 5) Bot (1 0)
+                               (Bot 0) (1 1) (Bot 1) 
+                               (0 Bot) (6 6) (0 1))))
+     (term #f))
+
+    (test-equal
+     (term (first-unhandled-d (1 1)
+                              ((1 Bot) (0 0) (7 7)
+                               (5 5) Bot (1 0)
+                               (1 1) (Bot 1) 
+                               (0 Bot) (6 6) (0 1))))
+     (term (Bot 0)))
 
     (test-results))
 
   (define (program-test-suite rr)
 
-    ;; FIXME: write more programs
-    
     ;; E-Freeze
     (test-->> rr
               (term
@@ -163,68 +270,64 @@
                (((l ((3 4) #t)))
                 (3 4))))
 
+    ;; Quasi-determinism with freezing.
+    (test-->> rr
+              (term
+               (() ;; empty store
+                (let ((x_1 new))
+                  (let par
+                      ((x_2 (let ((x_4 (put x_1 (3 Bot))))
+                              (freeze x_1 after ())))
+                       (x_3 (put x_1 (Bot 6))))
+                    x_2))))
+              (term
+               (((l ((3 6) #t)))
+                (3 6)))
+              (term
+               Error))
+
+    ;; Should deterministically raise an error, since it never uses
+    ;; freezing.
+    (test-->> rr
+              (term
+               (() ;; empty store
+                (let ((x_1 new))
+                  (let par
+                      ((x_2 (let ((x_4 (put x_1 (3 4))))
+                              ;; legal, incompatible 2-element
+                              ;; threshold set
+                              (get x_4 ((3 4) (6 6)))))
+                       (x_3 (put x_1 (6 6))))
+                    x_2))))
+              (term
+               Error))
+
+    ;; Should get stuck reducing, since ((3 Bot) (Bot 6)) is an
+    ;; illegal threshold set.  (Actually, this isn't quite right; such
+    ;; programs should be ruled out from the start somehow.)
+    (test-->> rr
+              (term
+               (() ;; empty store
+                (let ((x_1 new))
+                  (let par
+                      ((x_2 (get x_1 ((3 Bot) (Bot 6))))
+                       (x_3 (put x_1 (6 6))))
+                    x_2))))
+
+              ;; FIXME: Is there a way to just specify "gets stuck
+              ;; reducing"?  This overspecifies; I don't really care
+              ;; *how* it gets stuck.
+              (term
+               (((l ((6 6) #f)))
+                (((lambda (x_2)
+                    (lambda (x_3) x_2))
+                  (get l ((3 Bot) (Bot 6))))
+                 ()))))
+
     (test-results)))
 
 (module test-all racket
   (require (submod ".." test-suite))
   (test-all))
-
-;; -------------------------------------------------------------------
-
-;; Some tests that *should* work...
-
-;; TODO: don't use `with`.
-
-(module experimental-tests racket
-  (require redex/reduction-semantics)
-  (require (submod ".." language))
-
-  ;; Should be quasi-deterministic.
-  (test-->> rr
-            (term
-             (() ;; empty store
-              (let ((x_1 new))
-                (let par
-                    ((x_2 (freeze x_1 after () with (put x_1 ((3 Bot)))))
-                     (x_3 (put x_1 ((Bot 6)))))
-                  x_2))))
-            (term
-             (((l ((3 6) #t)))
-              ((3 6))))
-            (term
-             Error))
-
-  ;; Should deterministically raise an error, since it never uses
-  ;; freezing.
-  (test-->> rr
-            (term
-             (() ;; empty store
-              (let ((x_1 new))
-                (let par
-                    ((x_2 (let ((x_4 (put x_1 ((3 4)))))
-                            ;; legal, incompatible 2-element
-                            ;; threshold set
-                            (get x_4 ((3 4) (6 6)))))
-                     (x_3 (put x_1 ((6 6)))))
-                  x_2))))
-            (term
-             Error))
-
-  ;; Should get stuck reducing, since ((3 Bot) (Bot 6)) is an illegal
-  ;; threshold set.
-  (test-->> rr
-            (term
-             (() ;; empty store
-              (let ((x_1 new))
-                (let par
-                    ((x_2 (get x_1 ((3 Bot) (Bot 6))))
-                     (x_3 (put x_1 ((6 6)))))
-                  x_2))))
-            (term
-             (((l ((6 6) #f)))
-              (((lambda (x_2)
-                  (lambda (x_3) x_2))
-                (get l ((3 Bot) (Bot 6))))
-               ())))))
 
 
