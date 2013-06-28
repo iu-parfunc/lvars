4c5eb48e6f85fc680ccdd2cce9e2f0dd3f8f035b
Ryan Newton <rrnewton@gmail.com>
2013-06-27 21:10:44 -0400

    Oops, forgot to add a module.

---
+++ haskell-prototype/Control/LVish.hs
@@ -0,0 +1,7 @@
+
+-- | A module that reexports the default LVish scheduler.
+
+module Control.LVish (
+       module Control.LVish.SchedIdempotent
+       ) where
+import Control.LVish.SchedIdempotent
