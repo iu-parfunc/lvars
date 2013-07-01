cd838fabb3fd89664f815a0da733feba2f71b1f4
Ryan Newton <rrnewton@gmail.com>
2013-06-30 23:17:05 -0400

    WIP: parallel number parsing: introduce a proper notion of middle fragments

--- haskell-prototype/Util/PBBS.hs
+++ haskell-prototype/Util/PBBS.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns, OverloadedStrings #-}
+{-# LANGUAGE BangPatterns, OverloadedStrings, TemplateHaskell #-}
 
 -- | Utilities for reading PBBS data files, etc.
 
@@ -6,14 +6,18 @@ module Util.PBBS where
 
 import Control.Monad.Par.IO
 import Data.Word
+import Data.Maybe (fromJust)
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as M
 import qualified Data.ByteString as S
 import Data.ByteString.Unsafe (unsafeTail, unsafeHead)
+
 -- import qualified Data.ByteString.Word8      as S
 -- import qualified Data.ByteString.Lazy.Word8 as L
 
 import Test.HUnit
+import Test.Framework.Providers.HUnit
+import Test.Framework.TH (defaultMainGenerator)
 
 
 -- How many words shoud go in each continuously allocated vector?
@@ -22,13 +26,45 @@ chunkSize = 2 * 32768
 
 -- | A number that was interrupted in the middle.  The same datatype is used for left
 -- and right halves.
-data Fragment = None                
-              | Fragment {
-                numDigits :: {-# UNPACK #-} !Int,
+-- data Fragment = None                
+--               | RightFrag {
+--                 numDigits    :: {-# UNPACK #-} !Int,
+--                 partialParse :: {-# UNPACK #-} !Word
+--                 -- ^ The partialParse will need to be combined with the other half
+--                 -- through addition (shifting first if it represents a left-half).
+--                 }
+--               | MiddleFrag {
+--                 numDigits    :: {-# UNPACK #-} !Int,
+--                 partialParse :: {-# UNPACK #-} !Word
+--                 }
+--               | LeftFrag {
+--                 partialParse :: {-# UNPACK #-} !Word
+--                 }
+
+
+-- | This represents the rightmost portion of a decimal number that was interrupted
+-- in the middle.
+data RightFrag = RightFrag {
+                numDigits    :: {-# UNPACK #-} !Int,
                 partialParse :: {-# UNPACK #-} !Word
                 -- ^ The partialParse will need to be combined with the other half
                 -- through addition (shifting first if it represents a left-half).
                 }
+  deriving (Show,Eq,Ord,Read)
+           
+data LeftFrag = LeftFrag {-# UNPACK #-} !Word
+  deriving (Show,Eq,Ord,Read)
+           
+-- | A fragment from the middle of a number, (potentially) missing pieces on both ends.
+data MiddleFrag = MiddleFrag {-# UNPACK #-} !Int
+                             {-# UNPACK #-} !Word
+  deriving (Show,Eq,Ord,Read)
+
+
+-- | A sequence of parsed numbers with ragged edges.
+data PartialNums = Compound !(Maybe RightFrag) !(U.Vector Word) !(Maybe LeftFrag)
+                 | Single MiddleFrag
+  deriving (Show,Eq,Ord,Read)
 
 
 -- | Sequentially reads all the unsigned 64bit decimal (ASCII) numbers within a
@@ -37,31 +73,36 @@ data Fragment = None
 -- a count of how many digits were present in the first number (e.g. including
 -- leading zeros) if a number began on the very first character.  (Which makes it
 -- also possibly a fragment.)
-readWord64s :: Int -> S.ByteString -> IO (Maybe Word, U.Vector Word, Maybe Word)
+readWord64s :: Int -> S.ByteString -> IO PartialNums
 -- readWords :: Int -> S.ByteString -> IO (M.IOVector Word, Word)
 readWord64s charLimit bs | charLimit > S.length bs =
   error $ "readWords: asked for more characters ("++show charLimit++
           ") than are present in bytestring ("++ show (S.length bs)++")"
-readWord64s 0 bs = return (Nothing, U.empty, Nothing)
+readWord64s 0 bs = return$ Single (MiddleFrag 0 0)
 readWord64s charLimit bs = do
   initV <- M.new chunkSize
   let hd = S.head bs      
   (v,w,ind) <- scanfwd charLimit 0 initV hd (S.tail bs)
   v'        <- U.unsafeFreeze v
-  let pref = U.take ind v'
+  let pref  = U.take ind v'
+      rfrag = Just (RightFrag (fromJust$ S.findIndex (not . digit) bs) (U.head pref))
   if digit hd then
-    return (Just (U.head v'), U.drop 1 pref, w)
+    (if pref == U.empty
+     then case w of
+           Nothing  -> return$ Compound rfrag (U.tail pref) Nothing
+           Just (LeftFrag w) -> return$ Single$ MiddleFrag charLimit w
+     else return$ Compound rfrag (U.tail pref) w)
    else
-    return (Nothing, pref, w)
+    return$ Compound Nothing pref w
  where
    loop :: Int -> Int -> Word -> M.IOVector Word -> Word8 -> S.ByteString ->
-           IO (M.IOVector Word, Maybe Word, Int)
+           IO (M.IOVector Word, Maybe LeftFrag, Int)
    loop !lmt !ind !acc !vec !nxt !rst
      -- Extend the currently accumulating number in 'acc':
      | digit nxt =
        let acc' = (10*acc + (fromIntegral nxt-48)) in 
        if lmt == 1
-       then return (vec, Just acc', ind)
+       then return (vec, Just (LeftFrag acc'), ind)
        else loop (lmt-1) ind acc' vec (unsafeHead rst) (unsafeTail rst)
      | otherwise =
        do M.write vec ind acc
@@ -77,22 +118,24 @@ readWord64s charLimit bs = do
 
    digit nxt = nxt >= 48 && nxt <= 57
 
---   scanfwd !lmt !vec !nxt !rst =
---     | 
---     error "implement scanfwd"                                
-
-example =
-  readWord64s 10 ("123 456 789")
-  
---  loop 
-
-case_t1 = assertEqual "t1" (Just 123, U.fromList [], Nothing) =<<
+case_t1 :: IO ()
+case_t1 = assertEqual "t1" (Compound (Just (RightFrag 3 123)) (U.fromList []) Nothing) =<<
           readWord64s 4 ("123 4")
-case_t2 = assertEqual "t1" (Just 123, U.fromList [], Just 4) =<<
+case_t2 = assertEqual "t1" (Compound (Just (RightFrag 3 123)) (U.fromList []) (Just (LeftFrag 4))) =<<
           readWord64s 5 ("123 4")
-case_t3 = readWord64s 3 ("123")
-case_t4 = readWord64s 2 ("123")
-case_t5 = readWord64s 3 (" 123")
+case_t3 = assertEqual "t3" (Single (MiddleFrag 3 123)) =<<
+          readWord64s 3 ("123")
+case_t4 = assertEqual "t4" (Single (MiddleFrag 2 12)) =<<
+          readWord64s 2 ("123")
+case_t5 = assertEqual "t5" (Compound Nothing U.empty (Just (LeftFrag 12))) =<<
+          readWord64s 3 (" 123")
+
+case_t6 = assertEqual "t6"
+          (Compound (Just (RightFrag 3 23)) (U.fromList [456]) (Just (LeftFrag 78))) =<<
+          readWord64s 10 ("023 456 789")
+
+
+main = $(defaultMainGenerator)
 
 {-
 test :: Int -> Int -> Int -> S.ByteString -> Int
