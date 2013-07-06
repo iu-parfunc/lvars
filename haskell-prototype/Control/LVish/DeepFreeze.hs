abd07b0a955049839e6b3a45ab8d9615e705671c
Ryan Newton <rrnewton@gmail.com>
2013-07-06 10:03:52 -0400

    Prune speculative alternatives out of DeepFreeze

--- haskell-prototype/Control/LVish/DeepFreeze.hs
+++ haskell-prototype/Control/LVish/DeepFreeze.hs
@@ -16,6 +16,46 @@ import           Data.LVar.IVar
 import           Data.LVar.Set
 import           Data.Traversable
 
+--------------------------
+
+-- | This establishes an unrestricted *relation* between input and output types.  Thus
+-- it is powerful, but can be painful to use.  The input and output types of
+-- deepFreeze must be fully constrained at every call site.  This allows the user to
+-- potentially freeze a nested structure in various ways of their choosing.
+class DeepFreeze (from :: *) (to :: *) where
+  deepFreeze :: from -> Par to 
+
+instance (LVarData1 f, LVarData1 g) =>
+         DeepFreeze (f (g a)) (Snapshot f (Snapshot g a)) where
+  deepFreeze lvd = do
+    -- let fn = (fmap (\ (IVarSnap m) -> m)) . freezeIVar
+    x <- L.unsafeUnQPar$ freeze lvd               :: Par (Snapshot f (g a))
+    y <- traverseSnap (L.unsafeUnQPar . freeze) x :: Par (Snapshot f (Snapshot g a))
+    return y
+
+-- Inherit everything that regular freeze can do:
+instance LVarData1 f => DeepFreeze (f a) (Snapshot f a) where
+  deepFreeze = L.unsafeUnQPar . freeze
+
+--------------------------------------------------------------------------------
+-- IVar specific instances:
+--------------------------------------------------------------------------------
+
+-- Teach it how to freeze WITHOUT the annoying snapshot constructor:
+instance DeepFreeze (IVar a) (Maybe a) where
+  deepFreeze iv = do IVarSnap m <- L.unsafeUnQPar$ freeze iv
+                     return m
+
+instance DeepFreeze (IVar a) b =>
+         DeepFreeze (IVar (IVar a)) (Maybe b)
+  where
+    deepFreeze (from :: (IVar (IVar a))) = do
+      x <- L.unsafeUnQPar$ freezeIVar from       :: Par (Maybe (IVar a))
+      y <- traverse deepFreeze x :: Par (Maybe b)
+      return y
+
+--------------------------------------------------------------------------------
+-- Unit Tests
 --------------------------------------------------------------------------------
 
 test0 = do
@@ -70,95 +110,5 @@ test4a = test4
 -- uh... how is this one actually working?
 test4b :: IO (Maybe Int)
 test4b = test4
-
---------------------------
--- EXPERIMENTING  
-
--- | This establishes an unrestricted *relation* between input and output types.  Thus
--- it is powerful, but can be painful to use.  The input and output types of
--- deepFreeze must be fully constrained at every call site.  This allows the user to
--- potentially freeze a nested structure in various ways of their choosing.
-class DeepFreeze (from :: *) (to :: *) where
-  deepFreeze :: from -> Par to 
-
-
-#if 0
--- This much works:
-instance (Traversable f) =>                     
-         DeepFreeze (f (IVar a)) (f (Maybe a)) where
-  deepFreeze = traverse freezeIVar
-#elif 0
--- Or this, but not both of course:
-instance (LVarData1 f) =>
-         DeepFreeze (f (IVar a)) (Snapshot f (Maybe a)) where
-  deepFreeze lvd = do
-    -- let fn = (fmap (\ (IVarSnap m) -> m)) . freezeIVar
-    x <- freeze lvd                :: Par (Snapshot f (IVar a))
-    y <- traverseSnap freezeIVar x :: Par (Snapshot f (Maybe a))
-    return y
-
-#elif 1
-instance (LVarData1 f, LVarData1 g) =>
-         DeepFreeze (f (g a)) (Snapshot f (Snapshot g a)) where
-  deepFreeze lvd = do
-    -- let fn = (fmap (\ (IVarSnap m) -> m)) . freezeIVar
-    x <- L.unsafeUnQPar$ freeze lvd               :: Par (Snapshot f (g a))
-    y <- traverseSnap (L.unsafeUnQPar . freeze) x :: Par (Snapshot f (Snapshot g a))
-    return y
-
--- Inherit everything that regular freeze can do:
-instance LVarData1 f => DeepFreeze (f a) (Snapshot f a) where
-  deepFreeze = L.unsafeUnQPar . freeze
-
-#else
--- This version could retain a bit more flexibility....
-instance (LVarData1 f, DeepFreeze a b) =>
-         DeepFreeze (f a) (Snapshot f b) where
-  deepFreeze lvd = do
-    x <- freeze lvd                :: Par (Snapshot f a)
-    y <- traverseSnap deepFreeze x :: Par (Snapshot f (Snapshot g a))
-    return y
-instance DeepFreeze a a where
-   deepFreeze = return
--- instance LVarData1 f => DeepFreeze (f a) (Snapshot f a) where
---    deepFreeze = freeze
-#endif
-
-
---------------------------------------------------------------------------------
--- IVar specific instances:
---------------------------------------------------------------------------------
-
--- Teach it how to freeze WITHOUT the annoying snapshot constructor:
-instance DeepFreeze (IVar a) (Maybe a) where
-  deepFreeze iv = do IVarSnap m <- L.unsafeUnQPar$ freeze iv
-                     return m
-
--- instance DeepFreeze a b =>
---          DeepFreeze (IVar a) (Maybe b) where
---   deepFreeze iv = do IVarSnap m <- freeze iv
---                      fmap deepFreeze m
-                     
-#if 1
-instance DeepFreeze (IVar a) b =>
-         DeepFreeze (IVar (IVar a)) (Maybe b)
-  where
-    deepFreeze (from :: (IVar (IVar a))) = do
-      x <- L.unsafeUnQPar$ freezeIVar from       :: Par (Maybe (IVar a))
-      y <- traverse deepFreeze x :: Par (Maybe b)
-      return y
-#else
--- But how do we generalize to arbitrary combinations of DIFFERENT LVar types?
-instance (LVarData1 f, DeepFreeze (f a) (g a)) =>
-         DeepFreeze (f (IVar a)) (g (Maybe a))
-  where
-    deepFreeze (from :: (f (IVar a))) = do
-      x <- freeze from               :: Par (Snapshot f (IVar a))
---      y <- traverseSnap freezeIVar x :: Par (Snapshot f (Maybe a))
---      x <- deepFreeze from       :: Par (g (IVar a))
---      y <- traverse deepFreeze x :: Par (Maybe b)
---      return y
-      undefined
-#endif
 
 --------------------------------------------------------------------------------
