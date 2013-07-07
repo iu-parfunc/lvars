038c282e5dd7ae50abac472ddea936eae30d1e60
Aaron Turon <aaron.turon@gmail.com>
2013-07-07 22:31:38 +0200

    Added skip list implementation of concurrent maps.

---
+++ haskell-prototype/Data/Concurrent/SkipListMap.hs
@@ -0,0 +1,145 @@
+{-# LANGUAGE ExistentialQuantification, GADTs #-}
+
+-- | An implementation of concurrent finite maps based on skip lists.
+--
+-- Skip lists are a probabilistic data structure that roughly approximate
+-- balanced trees.  At the bottom layer is a standard linked list representation
+-- of a finite map.  Above this is some number of "index" lists that provide
+-- shortcuts to the layer below them.  When a key/value pair is added, it is
+-- always added to the bottom layer, and is added with exponentially decreasing
+-- probability to each index layer above it.
+--
+-- Skip lists are a very good match for lock-free programming, since the
+-- linearization point can be taken as insertion into the bottom list, and index
+-- nodes can be added *afterward* in a best-effort style (i.e., if there is
+-- contention to add them, we can simply walk away, with the effect that the
+-- probability of appearing in an index is partly a function of contention.)
+-- 
+-- To implement skip lists in Haskell, we use a GADT to represent the layers,
+-- each of which has a different type (since it indexes the layer below it).
+
+module Data.Concurrent.SkipListMap (
+  SLMap(), newSLMap, find, putIfAbsent, forPairs)
+where
+  
+import System.Random  
+  
+import Control.Monad  
+import Control.Monad.IO.Class
+  
+import Data.IORef
+import Data.Atomics
+import qualified Data.Concurrent.LinkedMap as LM
+
+-- | The GADT representation.  The type @t@ gives the type of nodes at a given
+-- level in the skip list.
+data SLMap_ k v t where
+  Bottom :: LM.LMap k v -> SLMap_ k v (LM.LMap k v)
+  Index  :: LM.LMap k (t, v) -> SLMap_ k v t -> SLMap_ k v (LM.LMap k (t, v))
+  
+data SLMap k v = forall t. SLMap (SLMap_ k v t) (LM.LMap k v)
+
+-- | Create a new skip list with the given number of levels.
+newSLMap :: Int -> IO (SLMap k v)
+newSLMap 0 = do
+  lm <- LM.newLMap
+  return $ SLMap (Bottom lm) lm
+newSLMap n = do 
+  SLMap slm lmBottom <- newSLMap (n-1)
+  lm <- LM.newLMap
+  return $ SLMap (Index lm slm) lmBottom
+
+-- | Attempt to locate a key in the map.
+find :: Ord k => SLMap k v -> k -> IO (Maybe v)      
+find (SLMap slm _) k = find_ slm Nothing k
+
+-- Helper for @find@.
+find_ :: Ord k => SLMap_ k v t -> Maybe t -> k -> IO (Maybe v)
+
+-- At the bottom level: just lift the find from LinkedMap
+find_ (Bottom m) shortcut k = do
+  searchResult <- LM.find (maybe m id shortcut) k
+  case searchResult of
+    LM.Found v      -> return $ Just v
+    LM.NotFound tok -> return Nothing
+    
+-- At an indexing level: attempt to use the index to shortcut into the level
+-- below.  
+find_ (Index m slm) shortcut k = do 
+  searchResult <- LM.find (maybe m id shortcut) k
+  case searchResult of 
+    LM.Found (_, v) -> 
+      return $ Just v   -- the key is in the index itself; we're outta here
+    LM.NotFound tok -> case LM.value tok of
+      Just (m', _) -> find_ slm (Just m') k     -- there's an index node
+                                                -- preceeding our key; use it to
+                                                -- shortcut into the level below.
+      
+      Nothing      -> find_ slm Nothing k       -- no smaller key in the index,
+                                                -- so start at the beginning of
+                                                -- the level below.
+      
+-- | Adds a key/value pair if the key is not present, all within a given monad.
+-- Returns the value now associated with the key in the map.
+putIfAbsent :: Ord k => MonadIO m => 
+               SLMap k v ->     -- ^ The map
+               IO Bool ->       -- ^ A coin tosser (should be thread-local)
+               k ->             -- ^ The key to lookup/insert
+               m v ->           -- ^ A computation of the value to insert
+               m v 
+putIfAbsent (SLMap slm _) gen k vc = 
+  putIfAbsent_ slm Nothing gen k vc $ \_ _ -> return ()
+
+-- Helper for putIfAbsent
+putIfAbsent_ :: Ord k => MonadIO m =>
+                SLMap_ k v t -> -- ^ The map    
+                Maybe t ->      -- ^ A shortcut into this skiplist level
+                IO Bool ->      -- ^ A coin tosser 
+                k ->            -- ^ The key to lookup/insert
+                m v ->          -- A computation of the value to insert
+                (t -> v -> IO ()) ->    -- A thunk for inserting into the higher
+                                        -- levels of the skiplist
+                m v
+                
+-- At the bottom level, we use a retry loop around the find/tryInsert functions
+-- provided by LinkedMap
+putIfAbsent_ (Bottom m) shortcut gen k vc install = retryLoop vc where 
+  -- The retry loop; ensures that vc is only executed once
+  retryLoop vc = do
+    searchResult <- liftIO $ LM.find (maybe m id shortcut) k
+    case searchResult of
+      LM.Found v      -> return v
+      LM.NotFound tok -> do
+        v <- vc
+        maybeMap <- liftIO $ LM.tryInsert tok v
+        case maybeMap of
+          Just m' -> do
+            liftIO $ install m' v         -- all set on the bottom level, now try indices
+            return v
+          Nothing -> retryLoop $ return v -- next time around, remember the value to insert
+          
+-- At an index level; try to shortcut into the level below, while remembering
+-- where we were so that we can insert index nodes later on
+putIfAbsent_ (Index m slm) shortcut gen k vc install = do          
+  searchResult <- liftIO $ LM.find (maybe m id shortcut) k
+  case searchResult of 
+    LM.Found (_, v) -> return v         -- key is in the index; bail out
+    LM.NotFound tok -> 
+      let install' mBelow v = do        -- to add an index node here,
+            flip <- gen                 -- first, see if we (probabilistically) should
+            when flip $ do 
+              maybeHere <- LM.tryInsert tok (mBelow, v)  -- then, try it!
+              case maybeHere of
+                Just mHere -> install mHere v  -- if we succeed, keep inserting
+                                               -- into the levels above us
+                              
+                Nothing -> return ()    -- otherwise, oh well; we tried.
+      in case LM.value tok of
+        Just (m', _) -> putIfAbsent_ slm (Just m') gen k vc install'
+        Nothing      -> putIfAbsent_ slm Nothing   gen k vc install'
+        
+-- | Concurrently iterate over all key/value pairs in the map within the given
+-- monad.  Inserts that arrive concurrently may or may not be included in the
+-- iteration.
+forPairs :: MonadIO m => SLMap k v -> (k -> v -> m ()) -> m ()
+forPairs (SLMap _ lm) f = LM.forPairs lm f
