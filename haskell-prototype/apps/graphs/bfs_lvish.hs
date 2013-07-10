f58f3e8830b76c321dfbac69db714f127ccd0b71
Ryan Newton <rrnewton@gmail.com>
2013-07-09 23:43:10 -0400

    Add sequential MIS which blows the others away

--- haskell-prototype/apps/graphs/bfs_lvish.hs
+++ haskell-prototype/apps/graphs/bfs_lvish.hs
@@ -6,14 +6,18 @@ import Data.Set as Set
 import Util.PBBS
 import Control.LVish
 import Control.LVish.Internal
+
 import Control.Monad
 import Control.Monad.Par.Combinator (parFor, InclusiveRange(..))
+import Control.Monad.ST
+import Control.Exception
 
 import Data.Word
 import Data.LVar.MaxCounter as C
 import Data.Traversable as T
 import Data.Time.Clock
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as M
 import qualified Data.Vector.Storable as UV
 import System.Mem (performGC)
 import System.Environment (getArgs)
@@ -180,12 +184,10 @@ flag_CHOSEN    = 1
 flag_NBRCHOSEN = 2
 
 {-# INLINE maximalIndependentSet #-}
--- | Uses a notion of priority writes.
 -- maximalIndependentSet :: ISet s NodeID -> Par d s (ISet s NodeID)  -- Operate on a subgraph
 -- maximalIndependentSet :: AdjacencyGraph -> Par d s (ISet s NodeID) -- Operate on a whole graph.
 maximalIndependentSet :: ParFor d s -> AdjacencyGraph -> Par d s (NatArray s Word8) -- Operate on a whole graph.
 maximalIndependentSet parFor gr@(AdjacencyGraph vvec evec) = do
-  logStrLn$ " [MIS] Beginning maximalIndependentSet "
   -- For each vertex, we record whether it is CHOSEN, not chosen, or undecided:
   let numVerts = U.length vvec
   flagsArr :: NatArray s Word8 <- newNatArray numVerts
@@ -216,6 +218,67 @@ maximalIndependentSet parFor gr@(AdjacencyGraph vvec evec) = do
       -- logStrLn$ " [MIS] processing node "++show ndIx++" nbrs "++show nds
       loop (U.length nds) nds ndIx  0
   return flagsArr
+
+-- DUPLICATE CODE: IStructure version.
+maximalIndependentSet2 :: ParFor d s -> AdjacencyGraph -> Par d s (IStructure s Word8) -- Operate on a whole graph.
+maximalIndependentSet2 parFor gr@(AdjacencyGraph vvec evec) = do
+  logStrLn$ " [MIS] Beginning maximalIndependentSet / Istructures"
+  -- For each vertex, we record whether it is CHOSEN, not chosen, or undecided:
+  let numVerts = U.length vvec
+  flagsArr <- newIStructure numVerts
+  let       
+      -- Here's the loop that scans through the neighbors of a node.
+      loop !numNbrs !nbrs !selfInd !i 
+        | i == numNbrs = thisNodeWins
+        | otherwise = do
+          -- logStrLn$ " [MIS]   ... on nbr "++ show i++" of "++show numNbrs
+          let nbrInd = fromIntegral$ nbrs U.! i -- Find our Nbr's NodeID
+              selfInd' = fromIntegral selfInd
+          -- If we got to the end of the neighbors below us, then we are NOT disqualified:
+          if nbrInd > selfInd
+            then thisNodeWins
+            else do
+              -- This should never block in a single-thread execution:
+              logStrLn (" [MIS] ! Getting on nbrInd "++show nbrInd)
+              nbrFlag <- ISt.get flagsArr (fromIntegral nbrInd)
+              logStrLn (" [MIS] ! Get completed on nbrInd "++show nbrInd)
+              if nbrFlag == flag_CHOSEN
+                then ISt.put_ flagsArr selfInd' flag_NBRCHOSEN
+                else loop numNbrs nbrs selfInd (i+1)
+        where
+          thisNodeWins = logStrLn (" [MIS] ! Node chosen: "++show selfInd) >> 
+                         ISt.put_ flagsArr (fromIntegral selfInd) flag_CHOSEN
+  parFor (0,numVerts) $ \ ndIx -> do 
+      let nds = nbrs gr (fromIntegral ndIx)
+      -- logStrLn$ " [MIS] processing node "++show ndIx++" nbrs "++show nds
+      loop (U.length nds) nds ndIx  0
+  return flagsArr
+
+
+-- | Sequential version.
+maximalIndependentSet3 :: AdjacencyGraph -> (U.Vector Word8)
+maximalIndependentSet3 gr@(AdjacencyGraph vvec evec) = U.create $ do
+  let numVerts = U.length vvec
+  flagsArr <- M.replicate numVerts 0
+  let loop !numNbrs !nbrs !selfInd !i 
+        | i == numNbrs = thisNodeWins
+        | otherwise = do
+          let nbrInd   = fromIntegral$ nbrs U.! i -- Find our Nbr's NodeID
+              selfInd' = fromIntegral selfInd
+          if nbrInd > selfInd
+            then thisNodeWins
+            else do
+              nbrFlag <- M.read flagsArr (fromIntegral nbrInd)
+              if nbrFlag == flag_CHOSEN
+                then M.write flagsArr selfInd' flag_NBRCHOSEN
+                else loop numNbrs nbrs selfInd (i+1)
+        where
+          thisNodeWins = M.write flagsArr (fromIntegral selfInd) flag_CHOSEN
+  for_ (0,numVerts) $ \ ndIx -> do 
+      let nds = nbrs gr (fromIntegral ndIx)
+      loop (U.length nds) nds ndIx 0
+  return flagsArr
+
 
   
 -- need to compute a fold over neighbors... some of which are bottom
@@ -356,8 +419,20 @@ main = do
             _ <- runParIO_ par
             return ()
 
-
-
+    ----------------------------------------
+    8 -> do putStrLn " ! Version 8: MIS only, with IStructures / parForL"
+            let par :: Par d0 s0 (IStructure s0 Word8)
+                par = maximalIndependentSet2 parForL gr
+            _ <- runParIO_ par
+            return ()
+    -- This version doesn't get the horrible parallel slowdown of version 5-7.
+    -- But alas, version 7 sequential is better.
+    -- And version 9 sequential is WAY better (>50X faster)
+  
+    ----------------------------------------
+    9 -> do putStrLn " ! Version 9: MIS only, sequential"
+            evaluate $ maximalIndependentSet3 gr
+            return ()
 
   t1 <- getCurrentTime
   putStrLn$ "Done"
