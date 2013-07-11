c8c39cc4dd2538a1715a20a757b2e28ae1d09a51
Ryan Newton <rrnewton@gmail.com>
2013-07-11 01:34:49 -0400

    Add the option of doing per-vertex work

---
+++ haskell-prototype/apps/graphs/Util.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE BangPatterns, OverloadedStrings #-}
+
+module Util where
+-- module Main where
+
+import           Control.Exception (evaluate)
+import           Control.Monad (forM_, when)
+import           Data.Word
+import           Data.IORef
+import           Data.List as L
+import           Data.List.Split (chunksOf)
+import qualified Data.IntSet as IS
+import qualified Data.ByteString.Lazy.Char8 as B
+import           Data.Time.Clock (getCurrentTime, diffUTCTime)
+import           Text.Printf (printf)
+import           System.Mem (performGC)
+import           System.IO.Unsafe (unsafePerformIO)
+import           System.Environment (getEnvironment,getArgs)
+import           System.CPUTime.Rdtsc (rdtsc)
+-- import           Data.Time.Clock (getCurrentTime)
+import           System.CPUTime  (getCPUTime)
+
+-- For representing graphs
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+
+type WorkRet = (Float)
+type WorkFn = (Node -> WorkRet)
+type Node = Int
+
+runAndReport act =
+  do (clocks_per_micro,freq) <- calibrate
+     startT <- rdtsc
+     t0 <- getCurrentTime
+     ----------------------------------------
+     -- wait_microsecs (1000 * 100)
+     act clocks_per_micro
+     ----------------------------------------     
+     t1 <- getCurrentTime
+     putStrLn $ "SELFTIMED " ++ show (diffUTCTime t1 t0) ++ "\n"
+
+     first <- readIORef first_hit
+     let first' = first - startT
+     putStrLn$"Start time in cycles: "++commaint startT      
+     putStrLn $ "First hit in raw clock cycles was " ++ commaint first'
+     let nanos = ((1000 * 1000 * 1000 * (fromIntegral first')) `quot` (fromIntegral freq :: Integer))
+     putStrLn $ " In nanoseconds: "++commaint nanos
+     putStrLn $ "FIRSTHIT " ++ show nanos
+
+
+calibrate :: IO (Double, Word64)
+calibrate = do 
+  freq <- measureFreq
+--  clocks_per_kilosin <- measureSin 1000
+  let clocks_per_micro :: Rational 
+      clocks_per_micro = (fromIntegral freq) / (1000 * 1000)
+--      kilosins_per_micro = clocks_per_micro / (fromIntegral clocks_per_kilosin)
+      -- numSins :: Rational       
+      -- numSins = (fromIntegral wrk) * kilosins_per_micro * 1000
+      -- numSins' = round numSins
+
+--      busy_waiter :: WorkFn
+--      busy_waiter n = unsafePerformIO $
+--        wait_microsecs (wrk * clocks_per_micro) n
+--        wait_sins numSins' n
+--        return (0,0)
+
+  printf "CPU Frequency: %s, clocks per microsecond %s\n"
+           (commaint freq) (commaint (round clocks_per_micro))
+
+--  printf "Time for 1K sins %s, Ksins per micro %s, numSins %s = %s\n"
+--         (show clocks_per_kilosin) 
+--         (show (fromRational kilosins_per_micro ::Double))
+--         (show numSins) (show numSins')
+
+  return (fromRational clocks_per_micro, freq)
+
+
+
+
+------------------------------------------------------------------------------------------
+
+first_hit :: IORef Word64
+first_hit = unsafePerformIO$ newIORef maxBound
+
+-- | Busy-wait for a certain number of clock cycles.
+wait_clocks :: Word64 -> IO Double -- (Float,Double)
+wait_clocks clocks = do
+  myT <- rdtsc
+  atomicModifyIORef' first_hit (\ t -> (min t myT,()))
+  let loop !n = do
+        now <- rdtsc
+        if now - myT >= clocks
+        then return n   
+        else loop (n+1)
+  cnt <- loop 0
+  return (fromIntegral cnt)
+
+
+-- Measure clock frequency, spinning rather than sleeping to try to
+-- stay on the same core.
+measureFreq :: IO Word64 -- What units is this in? -- LK
+measureFreq = do
+  let millisecond = 1000 * 1000 * 1000 -- picoseconds are annoying
+      -- Measure for how long to be sure?      
+--      measure = 200 * millisecond
+      measure = 1000 * millisecond      
+      scale :: Integer
+      scale = 1000 * millisecond `quot` measure
+  t1 <- rdtsc 
+  start <- getCPUTime
+  let loop :: Word64 -> Word64 -> IO (Word64,Word64)
+      loop !n !last = 
+       do t2 <- rdtsc 
+	  when (t2 < last) $
+	       putStrLn$ "COUNTERS WRAPPED "++ show (last,t2) 
+	  cput <- getCPUTime		
+	  if (cput - start < measure)
+	   then loop (n+1) t2
+	   else return (n,t2)
+  (n,t2) <- loop 0 t1
+  putStrLn$ "  Approx getCPUTime calls per second: "++ commaint (scale * fromIntegral n)
+  when (t2 < t1) $ 
+    putStrLn$ "WARNING: rdtsc not monotonically increasing, first "++show t1++" then "++show t2++" on the same OS thread"
+
+  return$ fromIntegral (fromIntegral scale * (t2 - t1))
+
+
+-- Readable large integer printing:
+commaint :: (Show a, Integral a) => a -> String
+commaint n = 
+   reverse $ concat $
+   intersperse "," $ 
+   chunksOf 3 $ 
+   reverse (show n)
+
--- haskell-prototype/apps/graphs/bfs_lvish.hs
+++ haskell-prototype/apps/graphs/bfs_lvish.hs
@@ -25,6 +25,8 @@ import System.Environment (getArgs)
 import System.Directory
 import System.Process
 
+import Util
+
 -- define DEBUG_CHECKS
 
 --------------------------------------------------------------------------------
@@ -210,6 +212,17 @@ maxDegreeI gr component = do
 
   return mc
 
+------------------------------------------------------------------------------------------
+-- A dummy per-node operation
+------------------------------------------------------------------------------------------  
+
+-- workEachNode :: (NatArray s Word8) -> (Word8 -> Par d s ()) -> Par d s (MaxCounter s)
+workEachNode :: Word64 -> (NatArray s Word8) -> Par d s ()
+workEachNode clocks component = do
+  NArr.forEach component $ \ nd flg ->
+    when (flg == 1) $ do
+      liftIO$ wait_clocks clocks
+      return ()
 
 ------------------------------------------------------------------------------------------
 -- Maximal Independent Set
@@ -380,12 +393,14 @@ main = do
   
   --------------------------------------------------------------------------------
   args <- getArgs
-  let (version,topo,size) = case args of
-                             [ver,tp,s] -> (ver, tp, read s)
-                             [ver,tp]   -> (ver, tp, 1000)
-                             [ver]      -> (ver, "grid", 1000) 
-                             []         -> ("bfsN","grid",1000)
-                             oth        -> error "Too many command line args!"
+  let (version,topo,size,wrksize::Double) =
+        case args of
+          [ver,tp,s,w] -> (ver, tp, read s, read w)
+          [ver,tp,s]   -> (ver, tp, read s, 0)
+          [ver,tp]     -> (ver, tp, 1000, 0)
+          [ver]        -> (ver, "grid", 1000, 0) 
+          []           -> ("bfsN","grid",1000, 0)
+          oth          -> error "Too many command line args!"
       existD d = do b <- doesDirectoryExist d
                     return$ if b then (Just d) else Nothing
                     
@@ -443,126 +458,143 @@ main = do
   -- writeFile "/tmp/debug" (show gr)
   -- putStrLn$ "Dumped parsed graph to /tmp/debug"  
   
-  t0 <- getCurrentTime
-  case version of
+  runAndReport $ \ clocks_per_micro ->
+    case version of
+      ----------------------------------------
+      "bfsS" -> do 
+                   putStrLn " ! Version 2: BFS only, with sets "
+                   let -- par2 :: Par d0 s0 (ISet s0 NodeID)
+                       -- par2 :: Par d0 s0 ()
+                       par2 = do comp <- bfs_async gr 0
+                                 waitSize numVerts comp -- A proxy for completeness... assumes fully connected graph.
+                                 return comp
+                   _ <- runParIO_ par2
+                   -- set:: Snapshot ISet NodeID <- runParThenFreezeIO par2
+                   -- let ISetSnap s = set                                          
+                   -- putStrLn$ "Connected component, set size "++show (Set.size s)
+                   return ()
 
-    ----------------------------------------
-    "bfsS" -> do 
-                 putStrLn " ! Version 2: BFS only, with sets "
-                 let -- par2 :: Par d0 s0 (ISet s0 NodeID)
-                     -- par2 :: Par d0 s0 ()
-                     par2 = do comp <- bfs_async gr 0
-                               waitSize numVerts comp -- A proxy for completeness... assumes fully connected graph.
-                               return comp
-                 _ <- runParIO_ par2
-                 -- set:: Snapshot ISet NodeID <- runParThenFreezeIO par2
-                 -- let ISetSnap s = set                                          
-                 -- putStrLn$ "Connected component, set size "++show (Set.size s)
-                 return ()
-            
-    ----------------------------------------
-    "bfsI" -> do putStrLn " ! Version 3: BFS only, with IStructures "
-                 let -- par2 :: Par d0 s0 (ISet s0 NodeID)
-                     par3 :: Par d0 s0 (IStructure s0 Bool)
-                     par3 = bfs_async_arr gr 0
-                 --  set:: Snapshot ISet NodeID <- runParThenFreezeIO par2
-                 _ <- runParIO_ par3
-                 return ()
+      ----------------------------------------
+      "bfsI" -> do putStrLn " ! Version 3: BFS only, with IStructures "
+                   let -- par2 :: Par d0 s0 (ISet s0 NodeID)
+                       par3 :: Par d0 s0 (IStructure s0 Bool)
+                       par3 = bfs_async_arr gr 0
+                   --  set:: Snapshot ISet NodeID <- runParThenFreezeIO par2
+                   _ <- runParIO_ par3
+                   return ()
 
-    ----------------------------------------
-    "bfsN" -> do putStrLn " ! Version 4: BFS only, with NatArrays "
-                 let -- par2 :: Par d0 s0 (ISet s0 NodeID)
-                     par4 :: Par d0 s0 (NatArray s0 Word8)
-                     par4 = bfs_async_arr2 gr 0
-                 --  set:: Snapshot ISet NodeID <- runParThenFreezeIO par2
-                 _ <- runParIO_ par4
-                 return ()
+      ----------------------------------------
+      "bfsN" -> do putStrLn " ! Version 4: BFS only, with NatArrays "
+                   let -- par2 :: Par d0 s0 (ISet s0 NodeID)
+                       par4 :: Par d0 s0 (NatArray s0 Word8)
+                       par4 = bfs_async_arr2 gr 0
+                   --  set:: Snapshot ISet NodeID <- runParThenFreezeIO par2
+                   _ <- runParIO_ par4
+                   return ()
 
-    ----------------------------------------
-    "misN1" -> do 
-            putStrLn " ! Version 5: MIS only, with NatArrays / parForSimple"
-            let par :: Par d0 s0 (NatArray s0 Word8)
-                par = maximalIndependentSet parForSimple gr
+      ----------------------------------------
+      "misN1" -> do 
+              putStrLn " ! Version 5: MIS only, with NatArrays / parForSimple"
+              let par :: Par d0 s0 (NatArray s0 Word8)
+                  par = maximalIndependentSet parForSimple gr
 #ifdef DEBUG_CHECKS
-            NatArraySnap (x :: UV.Vector Word8) <- runParThenFreezeIO par
-            putStrLn$ "MIS: result prefix: "++show (UV.take 100 x)
-            putStrLn$ "MIS: number of vertices in result: "++show (UV.sum (UV.filter (==1) x))
+              NatArraySnap (x :: UV.Vector Word8) <- runParThenFreezeIO par
+              putStrLn$ "MIS: result prefix: "++show (UV.take 100 x)
+              putStrLn$ "MIS: number of vertices in result: "++show (UV.sum (UV.filter (==1) x))
 #else
-            _ <- runParIO_ par
+              _ <- runParIO_ par
 #endif
-            return ()
+              return ()
 
-    ----------------------------------------
-    "misN2" -> do 
-            putStrLn " ! Version 6: MIS only, with NatArrays / parForTree"
-            let par :: Par d0 s0 (NatArray s0 Word8)
-                par = maximalIndependentSet parForTree gr
-            _ <- runParIO_ par
-            return ()
+      ----------------------------------------
+      "misN2" -> do 
+              putStrLn " ! Version 6: MIS only, with NatArrays / parForTree"
+              let par :: Par d0 s0 (NatArray s0 Word8)
+                  par = maximalIndependentSet parForTree gr
+              _ <- runParIO_ par
+              return ()
 
-    ----------------------------------------
-    "misN3" -> do 
-            putStrLn " ! Version 7: MIS only, with NatArrays / parForL"
-            let par :: Par d0 s0 (NatArray s0 Word8)
-                par = maximalIndependentSet parForL gr
-            _ <- runParIO_ par
-            return ()
+      ----------------------------------------
+      "misN3" -> do 
+              putStrLn " ! Version 7: MIS only, with NatArrays / parForL"
+              let par :: Par d0 s0 (NatArray s0 Word8)
+                  par = maximalIndependentSet parForL gr
+              _ <- runParIO_ par
+              return ()
 
-    ----------------------------------------
-    "misI3" -> do 
-            putStrLn " ! Version 8: MIS only, with IStructures / parForL"
-            let par :: Par d0 s0 (IStructure s0 Word8)
-                par = maximalIndependentSet2 parForL gr
-            _ <- runParIO_ par
-            return ()
-    -- This version doesn't get the horrible parallel slowdown of version 5-7.
-    -- But alas, version 7 sequential is better.
-    -- And version 9 sequential is WAY better (>50X faster)
-  
-    ----------------------------------------
-    "misSeq" -> do 
-            putStrLn " ! Version 9: MIS only, sequential"
-            evaluate $ maximalIndependentSet3 gr
-            return ()
+      ----------------------------------------
+      "misI3" -> do 
+              putStrLn " ! Version 8: MIS only, with IStructures / parForL"
+              let par :: Par d0 s0 (IStructure s0 Word8)
+                  par = maximalIndependentSet2 parForL gr
+              _ <- runParIO_ par
+              return ()
+      -- This version doesn't get the horrible parallel slowdown of version 5-7.
+      -- But alas, version 7 sequential is better.
+      -- And version 9 sequential is WAY better (>50X faster)
+
+      ----------------------------------------
+      "misSeq" -> do 
+              putStrLn " ! Version 9: MIS only, sequential"
+              evaluate $ maximalIndependentSet3 gr
+              return ()
 
 
-    ----------------------------------------
-    "bfsN_misI" -> do 
-            putStrLn " ! Version 10: BFS and then MIS w/ NatArrays/IStructure"
-            let par :: Par d0 s0 (IStructure s0 Word8)
-                par = do natarr <- bfs_async_arr2 gr 0
-                         maximalIndependentSet4 gr natarr
-            _ <- runParIO_ par
-            return ()
+      ----------------------------------------
+      "bfsN_misI" -> do 
+              putStrLn " ! Version 10: BFS and then MIS w/ NatArrays/IStructure"
+              let par :: Par d0 s0 (IStructure s0 Word8)
+                  par = do natarr <- bfs_async_arr2 gr 0
+                           maximalIndependentSet4 gr natarr
+              _ <- runParIO_ par
+              return ()
 
-    ----------------------------------------
-    "bfsN_misI_deg" -> do 
-            putStrLn " ! Version 10: BFS, MIS, and maxDegree"
-            let par :: Par d0 s0 (MaxCounter s0)
-                par = do natarr <- bfs_async_arr2 gr 0
-                         narr2 <- maximalIndependentSet4 gr natarr
-                         maxDegreeI gr narr2
-            mx <- runParThenFreezeIO par
-            putStrLn$ "Max degree in MIS was: "++show(mx::Int)
+      ----------------------------------------
+      "bfsN_misI_deg" -> do 
+              putStrLn " ! Version 11: BFS, MIS, and maxDegree"
+              let par :: Par d0 s0 (MaxCounter s0)
+                  par = do natarr <- bfs_async_arr2 gr 0
+                           narr2 <- maximalIndependentSet4 gr natarr
+                           maxDegreeI gr narr2
+              mx <- runParThenFreezeIO par
+              putStrLn$ "Max degree in MIS was: "++show(mx::Int)
 
-    ----------------------------------------
-    "?" -> do
-            putStrLn " ! Version 1: work in progress testing combinations of graph ops..."
-            let par1 :: Par d0 s0 (MaxCounter s0, ISet s0 NodeID)
-                par1 = do component <- bfs_async gr 0
-                          liftIO$ putStrLn "Got component..."
-                          mc <- maxDegreeS gr component    
-                          return (mc,component)            
-            (maxdeg::Int, set:: Snapshot ISet NodeID) <- runParThenFreezeIO2 par1
-            putStrLn$ "Processing finished, max degree was: "++show maxdeg
-            let ISetSnap s = set
-            putStrLn$ "Connected component, set size "++show (Set.size s)
 
-    oth -> error$"Unknown benchmark mode "++oth
+      ----------------------------------------
+      "bfsN_work" -> do
+              putStrLn " ! Version 12: BFS and per-vertex work"
+              let par :: Par d0 s0 ()
+                  par = do natarr <- bfs_async_arr2 gr 0
+                           workEachNode (round (wrksize * clocks_per_micro)) natarr
+              _ <- runParIO_ par
+              return ()
 
-  t1 <- getCurrentTime
+      ----------------------------------------
+      -- "bfsN_barrier_work" -> do
+      --         putStrLn " ! Version 13: BFS, barrier, and per-vertex work"
+      --         let par :: Par d0 s0 ()
+      --             par = do natarr <- bfs_async_arr2 gr 0
+      --                      workEachNode (round (32 * clocks_per_micro)) natarr
+      --         _ <- runParIO_ par
+      --         return ()
+
+
+      ----------------------------------------
+      "?" -> do
+              putStrLn " ! Version 1: work in progress testing combinations of graph ops..."
+              let par1 :: Par d0 s0 (MaxCounter s0, ISet s0 NodeID)
+                  par1 = do component <- bfs_async gr 0
+                            liftIO$ putStrLn "Got component..."
+                            mc <- maxDegreeS gr component    
+                            return (mc,component)            
+              (maxdeg::Int, set:: Snapshot ISet NodeID) <- runParThenFreezeIO2 par1
+              putStrLn$ "Processing finished, max degree was: "++show maxdeg
+              let ISetSnap s = set
+              putStrLn$ "Connected component, set size "++show (Set.size s)
+
+      oth -> error$"Unknown benchmark mode "++oth
+
   putStrLn$ "Done"
-  putStrLn$ "SELFTIMED: "++show (diffUTCTime t1 t0)
   
 
 
