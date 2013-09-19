4127f096285243ded114e690340c2bd274f089d1
toddaaro <git@opprobrio.us>
2013-09-19 17:45:17 -0400

    loads and has some construction stuff

--- haskell/lattice-par/Data/Concurrent/SBag.hs
+++ haskell/lattice-par/Data/Concurrent/SBag.hs
@@ -1,15 +1,17 @@
 module Data.Concurrent.SBag(SBag, new, insert, tryGetAny) where
 
-import Data.Array.IO
+import qualified Data.Vector.Mutable as V
+import qualified Data.Vector as IV
 import GHC.Conc
 
 blockSize = 4
 
-type Array a = IOUArray Int a
+type Array a = V.IOVector (Maybe a)
 
 data SBag e = 
-  SBag { globalHeadBlock :: Array (Block e),
+  SBag { globalHeadBlock :: (V.IOVector (Maybe (Block e))),
          numThreads :: Int,
+         initializedThreads :: (V.IOVector Bool),
          threadBlock :: Array (Block e),
          stealBlock :: Array (Block e),
          stealPrev :: Array (Block e),
@@ -26,10 +28,47 @@ data BlockPtr e =
   NullBlockPtr
 
 data Block e = 
-  Block { array :: (IOUArray Int e), 
-          notifyAdd :: (IOUArray Int Bool),
+  Block { array :: Array e, 
+          notifyAdd :: Array Bool,
           blockPtr :: BlockPtr e }
 
-new = 120
+new :: IO (SBag a)
+new = do
+  let n = numCapabilities
+  globalHeadBlock <- V.replicate n Nothing
+  initializedThreads <- V.replicate n False
+  threadBlock <- V.replicate n Nothing
+  stealBlock <- V.replicate n Nothing
+  stealPrev <- V.replicate n Nothing
+  foundAdd <- V.replicate n Nothing
+  threadHead <- V.replicate n Nothing
+  stealHead <- V.replicate n Nothing
+  stealIndex <- V.replicate n Nothing
+  return SBag {
+    globalHeadBlock = globalHeadBlock,
+    numThreads = n,
+    initializedThreads = initializedThreads,
+    threadBlock = threadBlock,
+    stealBlock = stealBlock,
+    stealPrev = stealPrev,
+    foundAdd = foundAdd,
+    threadHead = threadHead,
+    stealHead = stealHead,
+    stealIndex = stealIndex }
+
+initThread :: SBag a -> IO () 
+initThread bag = do
+  id <- currentCap
+  hb <- V.read (globalHeadBlock bag) id
+  V.write (threadBlock bag) id hb
+
 insert = 120
 tryGetAny = 120
+
+numCap = numCapabilities
+
+currentCap :: IO Int
+currentCap = do 
+  id <- myThreadId
+  res <- threadCapability id
+  return $ fst res
