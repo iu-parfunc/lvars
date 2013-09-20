e66f43e9bedd215bcf90d28631a8e99b6774b5a2
Lindsey Kuper <lindsey@composition.al>
2013-09-20 13:13:53 -0400

    Modernize benchmark.sh.

--- haskell/fhpc13-lvars/benchmarks/benchmark.sh
+++ haskell/fhpc13-lvars/benchmarks/benchmark.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
+# Just a utility script to handle building and running the benchmarks.
+
+# LK: TODO: figure out if HSBencher needs/uses the TRIALS env var.
+
 set -e
 
 make rand_data
 
-export THREADS="1 2 3 4"
-export TRIALS=5
-
 if [ $(hostname) = "hive.soic.indiana.edu" ];
 then
     lock_for_experiments 1000
-    export THREADS="1 2 4 8 16 32"
-    export TRIALS=5
+	export TRIALS=5
 fi
 
-if [ $(hostname) = "lenny" ]; # Lindsey's laptop
+if [ $(hostname) = "lenny" || $(hostname) = "landin.local" ]; # Lindsey's machines
 then
-    export THREADS="1 2 4"
     export TRIALS=1
 fi
-
 
 make benchmark.run
 ./benchmark.run
