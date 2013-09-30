8eb15cea9a7a26039b06484941b18348000e5223
Lindsey Kuper <lindsey@composition.al>
2013-09-30 17:30:18 -0400

    Clean up example code a little.

--- haskell/lvish/Control/LVish.hs
+++ haskell/lvish/Control/LVish.hs
@@ -23,23 +23,22 @@
 
   Here is a self-contained example that writes the same value to @num@
   twice and deterministically prints @4@ instead of raising an error, as
-  it would if @num@ were a traditional IVar rather than an LVar.
+  it would if @num@ were a traditional IVar rather than an LVar. (You
+  will need to compile using the `-XDataKinds` extension.)
 
 > {-# LANGUAGE DataKinds #-}
-> 
 > import Control.LVish  -- Generic scheduler; works with any lattice.
 > import Data.LVar.IVar -- The particular lattice in question.
 > 
 > p :: Par Det s Int
 > p = do
 >   num <- new
->   fork $ do put num 4
->   fork $ do put num 4
->   v <- get num
->   return v
+>   fork $ put num 4
+>   fork $ put num 4
+>   get num
 > 
 > main = do
->   putStr $ show $ runPar $ p
+>   print $ runPar $ p
 
  -}
 
