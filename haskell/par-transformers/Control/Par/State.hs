55f897ce875d7916ce4e5a58a20e7c5511543e82
Ryan Newton <rrnewton@gmail.com>
2013-10-07 01:02:58 -0400

    Check in an adapted state transformer

---
+++ haskell/par-transformers/Control/Par/State.hs
@@ -0,0 +1,111 @@
+{-# LANGUAGE ScopedTypeVariables, FlexibleInstances,
+             MultiParamTypeClasses, UndecidableInstances, CPP #-}
+{-# LANGUAGE TypeFamilies #-}
+
+-- | This module provides a notion of (Splittable) State that is
+--   compatible with any Par monad.
+--
+--   This module provides instances that make StateT-transformed
+--   monads into valid Par monads.
+
+module Control.Par.State
+  (
+    -- * Class for state split at fork points
+   SplittableState(..)
+   -- * Also note exported instances.
+  )
+  where
+
+import Control.Monad
+import Control.Monad.Trans
+import qualified Control.Monad.Trans.State.Strict as S
+import qualified Control.Monad.Trans.State.Lazy as SL
+
+-- import qualified Control.Monad.Par.Class as PC
+import qualified Control.Par.Class as PC
+
+---------------------------------------------------------------------------------
+--- Make Par computations with state work.
+--- (TODO: move these instances to a different module.)
+
+-- | A type in `SplittableState` is meant to be added to a Par monad
+--   using StateT.  It works like any other state except at `fork`
+--   points, where the runtime system splits the state using `splitState`.
+--
+--   Common examples for applications of `SplittableState` would
+--   include (1) routing a splittable random number generator through
+--   a parallel computation, and (2) keeping a tree-index that locates
+--   the current computation within the binary tree of `fork`s.
+--   Also, it is possible to simply duplicate the state at all fork points,
+--   enabling "thread local" copies of the state.
+--
+--   The limitation of this approach is that the splitting method is
+--   fixed, and the same at all `fork` points.
+class SplittableState a where
+  splitState :: a -> (a,a)
+
+----------------------------------------------------------------------------------------------------
+-- Strict State:
+
+-- | Adding State to a `ParFuture` monad yields another `ParFuture` monad.
+instance (SplittableState s, PC.ParFuture p)
+      =>  PC.ParFuture (S.StateT s p)
+ where
+  type Future (S.StateT s p)        = PC.Future p
+  type FutContents (S.StateT s p) a = PC.FutContents p a
+  get = lift . PC.get
+  spawn_ (task :: S.StateT s p ans) =
+    do s <- S.get
+       let (s1,s2) = splitState s
+       S.put s2                               -- Parent comp. gets one branch.
+       lift$ PC.spawn_ $ S.evalStateT task s1   -- Child the other.
+
+-- | Likewise, adding State to a `ParIVar` monad yield s another `ParIVar` monad.
+instance (SplittableState s, PC.ParIVar p)
+      =>  PC.ParIVar (S.StateT s p)
+ where
+  fork (task :: S.StateT s p ()) =
+              do s <- S.get
+                 let (s1,s2) = splitState s
+                 S.put s2
+                 lift$ PC.fork $ do S.runStateT task s1; return ()
+
+  new      = lift PC.new
+  put_ v x = lift$ PC.put_ v x
+  newFull_ = lift . PC.newFull_
+
+----------------------------------------------------------------------------------------------------
+-- Lazy State:
+
+-- <DUPLICATE_CODE>
+
+-- | Adding State to a `ParFuture` monad yield s another `ParFuture` monad.
+instance (SplittableState s, PC.ParFuture p)
+      =>  PC.ParFuture (SL.StateT s p)
+ where
+  type Future (SL.StateT s p)        = PC.Future p
+  type FutContents (SL.StateT s p) a = PC.FutContents p a   
+  get = lift . PC.get
+  spawn_ (task :: SL.StateT s p ans) =
+    do s <- SL.get
+       let (s1,s2) = splitState s
+       SL.put s2                               -- Parent comp. gets one branch.
+       lift$ PC.spawn_ $ SL.evalStateT task s1   -- Child the other.
+
+-- | Likewise, adding State to a `ParIVar` monad yield s another `ParIVar` monad.
+instance (SplittableState s, PC.ParIVar p)
+      =>  PC.ParIVar (SL.StateT s p)
+ where
+  fork (task :: SL.StateT s p ()) =
+              do s <- SL.get
+                 let (s1,s2) = splitState s
+                 SL.put s2
+                 lift$ PC.fork $ do SL.runStateT task s1; return ()
+
+  new      = lift PC.new
+  put_ v x = lift$ PC.put_ v x
+  newFull_ = lift . PC.newFull_
+
+
+-- </DUPLICATE_CODE>
+
