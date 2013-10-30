86d396646671f7b284f11a343522dd1cc1b747f9
Ryan Newton <rrnewton@gmail.com>
2013-10-30 18:21:48 -0400

    WIP: fleshing out the CancelT LVarSched instance

--- haskell/par-transformers/Control/LVish/CancelT.hs
+++ haskell/par-transformers/Control/LVish/CancelT.hs
@@ -22,6 +22,8 @@ import Control.Par.Class as PC
 newtype CancelT m a = CancelT ((StateT CState m) a)
   deriving (Monad, Functor)
 
+unCancelT (CancelT m) = m
+
 -- | Each computation has a boolean flag that stays True while it is still live.
 --   Also, the state for one computation is linked to the state of children, so that
 --   cancellation may be propagated transitively.
@@ -30,9 +32,11 @@ newtype CState = CState (IORef CPair)
 instance MonadTrans CancelT where
   lift m = CancelT (lift m)
 
-data CPair = CPair !Bool ![CState]
+-- TODO/FIXME: Replace MonadIO with something the user can't safely access.
+instance MonadIO m => MonadIO (CancelT m) where
+  liftIO m = CancelT (lift (liftIO m))
 
--- TODO: Replace MonadIO with something the user can't safely access.
+data CPair = CPair !Bool ![CState]
 
 poll :: (MonadIO m, LVarSched m) => CancelT m Bool
 poll = CancelT$ do
@@ -44,23 +48,43 @@ poll = CancelT$ do
 cancelMe :: CancelT m ()
 cancelMe = undefined
 
+cancelMe' :: StateT CState t ()
+cancelMe' = unCancelT cancelMe
+
 instance (MonadIO m, LVarSched m) => LVarSched (CancelT m) where
   type LVar (CancelT m) a d = LVar m a d
---    -- | An alternate form of the monad for quasi-deterministic computations.
---    type QPar m :: * -> *
+
+  type QPar (CancelT m) = CancelT (QPar m)
 
   type GetSession (CancelT m) = GetSession m
 
---    forkLV :: m () -> m ()
-  forkLV = do
-    -- Create new child state..
-    undefined
+  forkLV (CancelT act) = CancelT$ do
+--    b <- poll   -- Tradeoff: we could poll once before the atomic op.
+--    when b $ do
+    CState parentRef <- S.get
+    -- Create new child state:
+    childRef <- liftIO$ newIORef (CPair True [])    
+    live <- liftIO $ 
+      atomicModifyIORef' parentRef $ \ orig@(CPair bl ls) ->
+        if bl then
+          -- Extend the tree by pointing to our child:
+          (CPair True (CState childRef : ls), True)
+        else -- The current thread has already been canceled: DONT fork:
+          (orig, False)
+    if live then
+       lift $ forkLV (evalStateT act (CState childRef))
+      else cancelMe'
     
---    newLV :: Proxy (m (),a,d) -> IO a -> m (LVar m a d)        
   newLV (_:: Proxy(CancelT m (),a,d)) act =
     lift$ newLV (Proxy::Proxy(m (),a,d)) act
   
 --    stateLV :: (LVar m a d) -> (Proxy (m d), a)
+  stateLV lvar =
+--  stateLV lvar = -- (lvar :: LVar m a d) = 
+    let -- (_::Proxy(m d), a) = stateLV lvar
+    in undefined
+       -- (lvar:: (LVar m a d ~ LVar (CancelT m) a d) => LVar m a d)
+     -- (Proxy::Proxy((CancelT m) d), a)
 
   putLV lv putter = do 
      b <- poll
