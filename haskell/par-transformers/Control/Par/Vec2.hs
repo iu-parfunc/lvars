c5225cfc203b77e93a5dd15a92fdab88e091ca2e
toddaaro <github@opprobrio.us>
2013-10-30 14:35:50 -0700

    finished helpers for left vector

--- haskell/par-transformers/Control/Par/Vec2.hs
+++ haskell/par-transformers/Control/Par/Vec2.hs
@@ -43,95 +43,82 @@ import Prelude hiding (read, length, drop, take)
 
 --------------------------------------------------------------------------------
 
-type ParVec2 s1 elt1 elt2 parM ans =
-     ParST (STTup2 (MVectorFlp elt1) (MVectorFlp elt2) s1) parM ans
+type ParVec2 s1 eltL eltR parM ans =
+     ParST (STTup2 (MVectorFlp eltL) (MVectorFlp eltR) s1) parM ans
 
 -- | Restricted version of `runParST` which initialized the state with a single,
 -- boxed vector of a given size.  All elements start uninitialized.
-runParVec2 :: forall elt1 elt2 parM ans . (ParThreadSafe parM) => 
+runParVec2 :: forall eltL eltR parM ans . (ParThreadSafe parM) => 
              (Int,Int)
-             -> (forall s1 . ParVec2 s1 elt1 elt2 parM ans)
+             -> (forall s1 . ParVec2 s1 eltL eltR parM ans)
              -> parM ans
 runParVec2 (size1,size2) comp = 
   runParST (error "runParVec -- this initial value should be unused.") $ do 
-    vec1 <- liftST $ MV.new size1
-    vec2 <- liftST $ MV.new size2
-    S.put (STTup2 (VFlp vec1) (VFlp vec2))
+    vecL <- liftST $ MV.new size1
+    vecR <- liftST $ MV.new size2
+    S.put (STTup2 (VFlp vecL) (VFlp vecR))
     comp
-
-{-
--- | A shortcut for the common case of discharging both the vector computation and
--- the underlying `Par` monad at the same time.
-runParVec2' :: forall elt1 elt2 ans .
-              (Int,Int)
-              -> (forall s1 s2 . ParVec2 s1 elt1 elt2 'Det s2 ans)
-              -> ans
-runParVec2' pr m = runPar (runParVec2 pr m)
--}
 
 -- | Extract a pointer to the whole Vector in its normal, usable @STVector@ form.
 --   Use the `liftST` operator to act on it.
-reify :: (ParThreadSafe parM) => ParVec2 s1 elt1 elt2 parM (MV.STVector s1 elt1, MV.STVector s1 elt2)
+reify :: (ParThreadSafe parM) => ParVec2 s1 eltL eltR parM (MV.STVector s1 eltL, MV.STVector s1 eltR)
 reify = do
-  (STTup2 (VFlp vec1) (VFlp vec2)) <- S.get
-  return (vec1,vec2)
+  (STTup2 (VFlp vecL) (VFlp vecR)) <- S.get
+  return (vecL,vecR)
 
 --------------------------------------------------------------------------------
 
 -- | Write to the (implicit) vector state.
-writeL :: ParThreadSafe parM => Int -> elt1 -> ParVec2 s1 elt1 elt2 parM ()
+writeL :: ParThreadSafe parM => Int -> eltL -> ParVec2 s eltL eltR parM ()
 writeL ind val = do
-  STTup2 (VFlp vecl) (VFlp vecr) <- S.get
-  liftST$ MV.write vecl ind val
+  STTup2 (VFlp vecL) (VFlp vecR) <- S.get
+  liftST$ MV.write vecL ind val
 
-{-
 -- | Read the (implicit) vector state.
-readl :: ParThreadSafe parM => Int -> ParVec2 s1 elt1 elt2 parM elt
-readl ind = do
-  VFlp vec <- fst S.get
-  liftST$ MV.read vec ind 
+readL :: ParThreadSafe parM => Int -> ParVec2 s eltL eltR parM eltL
+readL ind = do
+  STTup2 (VFlp vecL) (VFlp vecR) <- S.get
+  liftST$ MV.read vecL ind 
 
 -- | Return the length of the (implicit) vector state.
-lengthl :: ParThreadSafe parM => ParVec2 s1 elt1 elt2 parM Int
---lengthl = (fst S.get) >>= (return . MV.length . unFlp)
-lengthl = do
-  VFlp vec <- fst S.get
-  return $ MV.length vec
+lengthL :: ParThreadSafe parM => ParVec2 s1 eltL eltR parM Int
+lengthL = do
+  STTup2 (VFlp vecL) (VFlp vecR) <- S.get
+  return $ MV.length vecL
 
 -- | Update the vector state by swapping two elements.
-swapl :: ParThreadSafe parM => Int -> Int -> ParVec2 s1 elt1 elt2 parM ()
--- swap x y = S.get >>= ((\ v -> MV.swap v x y) . unFlp)
-swapl x y = do
-  VFlp vec <- fst S.get
-  liftST$ MV.swap vec x y
+swapL :: ParThreadSafe parM => Int -> Int -> ParVec2 s1 eltL eltR parM ()
+swapL x y = do
+  STTup2 (VFlp vecL) (VFlp vecR) <- S.get  
+  liftST$ MV.swap vecL x y
 
 -- | Update the vector state by dropping the first @n@ elements.
-dropl :: ParThreadSafe parM => Int -> ParVec2 s1 elt1 elt2 parM ()
-dropl n = do 
-  VFlp vec <- fst S.get
-  S.put (VFlp (MV.drop n vec))
+dropL :: ParThreadSafe parM => Int -> ParVec2 s1 eltL eltR parM ()
+dropL n = do 
+  STTup2 (VFlp vecL) (VFlp vecR) <- S.get
+  S.put$ STTup2 (VFlp (MV.drop n vecL)) (VFlp vecR)
 
 -- | Update the vector state by taking the first @n@ elements, discarding the rest.
-takel :: ParThreadSafe parM => Int -> ParVec2 s1 elt1 elt2 parM ()
-takel n = do 
-  VFlp vec <- fst S.get
-  S.put (VFlp (MV.take n vec))
+takeL :: ParThreadSafe parM => Int -> ParVec2 s1 eltL eltR parM ()
+takeL n = do 
+  STTup2 (VFlp vecL) (VFlp vecR) <- S.get
+  S.put$ STTup2 (VFlp (MV.take n vecL)) (VFlp vecR)
 
 
 -- | Destructively replace the vector with a bigger vector, adding the given number
 -- of elements.  The new elements are uninitialized and will result in errors if
 -- read.
-growl :: ParThreadSafe parM => Int -> ParVec2 s1 elt1 elt2 parM ()
-growl n = do
-  VFlp vec <- fst S.get
-  vec' <- liftST$ MV.grow vec n
-  S.put (VFlp vec')
+growL :: ParThreadSafe parM => Int -> ParVec2 s1 eltL eltR parM ()
+growL n = do
+  STTup2 (VFlp vecL) (VFlp vecR) <- S.get
+  vecL' <- liftST$ MV.grow vecL n
+  S.put$ STTup2 (VFlp vecL') (VFlp vecR)
 
 -- | Mutate all the elements of the vector, setting them to the given value.
-setl :: ParThreadSafe parM => elt -> ParVec2 s1 elt1 elt2 parM ()
-setl val = do 
-  VFlp vec <- fst S.get
-  liftST $ MV.set vec val
--}
+setL :: ParThreadSafe parM => eltL -> ParVec2 s1 eltL eltR parM ()
+setL val = do 
+  STTup2 (VFlp vecL) (VFlp vecR) <- S.get
+  liftST $ MV.set vecL val
+
 --unFlp :: ST t t1 -> MV.MVector t1 t
 --unFlp (VFlp v) = v
