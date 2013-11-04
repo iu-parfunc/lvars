22b186369618bac223162bd681bb9c7198e391f2
toddaaro <github@opprobrio.us>
2013-11-04 10:51:39 -0800

    UVec helpers updated for ParST

--- haskell/par-transformers/Control/Par/ST/UVec.hs
+++ haskell/par-transformers/Control/Par/ST/UVec.hs
@@ -1,5 +1,15 @@
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE GADTs #-}
 
 -- | A convenience interface -- simply a restriction of `ParST` to the case
 --   of a single, boxed vector as the mutable state.
@@ -8,16 +18,16 @@
 --   "Data.Vector.Mutable", which operate directly on the implicit vector state
 --   threaded through the monad.
 
-module Control.LVish.ST.UVec
+module Control.Par.ST.UVec
        ( -- * A type alias for parallel computations with @Vector@ state
-         ParVec, 
-         runParVec, runParVec',
+         ParVecT, 
+         runParVecT,
 
          -- * Reexported from the generic interface
          forkSTSplit, liftPar, liftST, 
          
          -- * Retrieving an explict pointer to the Vector
-         reify, liftST,
+         reify,
                 
          -- * Common vector operations
          write, read, length, swap,
@@ -25,42 +35,34 @@ module Control.LVish.ST.UVec
        )
        where
 
-import Control.LVish (Par, Determinism(Det), runPar)
-import Control.LVish.ST
+import Control.Par.ST
 import qualified Control.Monad.State.Strict as S
 import qualified Data.Vector.Unboxed.Mutable as MU
 import Prelude hiding (read, length, drop, take)
+import Control.Par.Class.Unsafe (ParThreadSafe(unsafeParIO))
 
 --------------------------------------------------------------------------------
 -- Convenience interface ONE VECTOR state:
 --------------------------------------------------------------------------------
 
 -- | A type alias for parallel computations with an unboxed `MVector` state.
-type ParVec s1 elt det s2 ans = ParST (UVectorFlp elt s1) det s2 ans
+type ParVecT s1 elt parM ans = ParST (UVectorFlp elt s1) parM ans
 
 -- | Restricted version of `runParST` which initialized the state with a single,
 -- boxed vector of a given size.  All elements start uninitialized.
-runParVec :: forall elt s2 det ans . (MU.Unbox elt) =>
+runParVecT :: forall elt parM ans . (MU.Unbox elt, ParThreadSafe parM) =>
              Int
-             -> (forall s1 . ParVec s1 elt det s2 ans)
-             -> Par det s2 ans
-runParVec size comp = 
+             -> (forall s1 . ParVecT s1 elt parM ans)
+             -> parM ans
+runParVecT size comp = 
   runParST (error "runParVec -- this initial value should be unused.") $ do 
     vec <- liftST $ MU.new size
     S.put (UFlp vec)
     comp
 
--- | A shortcut for the common case of discharging both the vector computation and
--- the underlying `Par` monad at the same time.
-runParVec' :: forall elt ans . (MU.Unbox elt) =>
-              Int
-              -> (forall s1 s2 . ParVec s1 elt 'Det s2 ans)
-              -> ans
-runParVec' n m = runPar (runParVec n m)
-
 -- | Extract a pointer to the whole Vector in its normal, usable @STVector@ form.
 --   Use the `liftST` operator to act on it.
-reify :: ParVec s1 elt det s2 (MU.STVector s1 elt)
+reify :: ParThreadSafe parM => ParVecT s1 elt parM (MU.STVector s1 elt)
 reify = do
   UFlp vec <- S.get
   return vec
@@ -68,35 +70,35 @@ reify = do
 --------------------------------------------------------------------------------
 
 -- | Write to the (implicit) vector state.
-write :: Int -> elt -> ParVec s1 elt det s2 ()
+write :: ParThreadSafe parM => Int -> elt -> ParVecT s1 elt parM ()
 write ind val = do
   UFlp vec <- S.get
   liftST$ MU.write vec ind val
 
 -- | Read the (implicit) vector state.
-read :: Int -> ParVec s1 elt det s2 elt
+read :: ParThreadSafe parM => Int -> ParVecT s1 elt parM elt
 read ind = do
   UFlp vec <- S.get
   liftST$ MU.read vec ind 
 
 -- | Return the length of the (implicit) vector state.
-length :: MU.Unbox elt => ParVec s1 elt det s2 Int
+length :: (ParThreadSafe parM, MU.Unbox elt) => ParVecT s1 elt parM Int
 length = S.get >>= (return . MU.length . unFlp)
 
 -- | Update the vector state by swapping two elements.
-swap :: Int -> Int -> ParVec s1 elt det s2 ()
+swap :: ParThreadSafe parM => Int -> Int -> ParVecT s1 elt parM ()
 swap x y = do
   UFlp vec <- S.get
   liftST$ MU.swap vec x y
 
 -- | Update the vector state by dropping the first @n@ elements.
-drop :: Int -> ParVec s1 elt det s2 ()
+drop :: ParThreadSafe parM => Int -> ParVecT s1 elt parM ()
 drop n = do 
   UFlp vec <- S.get
   S.put (UFlp (MU.drop n vec))
 
 -- | Update the vector state by taking the first @n@ elements, discarding the rest.
-take :: Int -> ParVec s1 elt det s2 ()
+take :: ParThreadSafe parM => Int -> ParVecT s1 elt parM ()
 take n = do 
   UFlp vec <- S.get
   S.put (UFlp (MU.take n vec))
@@ -104,14 +106,14 @@ take n = do
 -- | Destructively replace the vector with a bigger vector, adding the given number
 -- of elements.  The new elements are uninitialized and will result in errors if
 -- read.
-grow :: Int -> ParVec s1 elt det s2 ()
+grow :: ParThreadSafe parM => Int -> ParVecT s1 elt parM ()
 grow n = do
   UFlp vec <- S.get
   vec' <- liftST$ MU.grow vec n
   S.put (UFlp vec')
 
 -- | Mutate all the elements of the vector, setting them to the given value.
-set :: elt -> ParVec s1 elt det s2 ()
+set :: ParThreadSafe parM => elt -> ParVecT s1 elt parM ()
 set val = do 
   UFlp vec <- S.get
   liftST $ MU.set vec val
