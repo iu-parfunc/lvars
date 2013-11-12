b364bdfe369ddbba110705162660ae3c3e571a54
toddaaro <github@opprobrio.us>
2013-11-12 12:53:16 -0800

    updated threshold

--- haskell/par-transformers/tests/MergeSort.hs
+++ haskell/par-transformers/tests/MergeSort.hs
@@ -111,7 +111,7 @@ mergeSort :: (ParThreadSafe parM, PC.FutContents parM (),
 mergeSort = do
   len <- V.lengthL
   
-  if len < 2 then do
+  if len < 8192 then do
     seqSortL
    else do  
     let sp = (len `quot` 2)              
