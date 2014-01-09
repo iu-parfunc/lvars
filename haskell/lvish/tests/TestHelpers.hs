75e3baf43d81cc864f738a94ecdb1fc4795878f6
Ryan Newton <rrnewton@gmail.com>
2014-01-09 12:40:57 -0500

    Factor out Lvish-specific bits of TestHelpers. This fixes a break in par-collections test-suite.

--- haskell/lvish/tests/TestHelpers.hs
+++ haskell/lvish/tests/TestHelpers.hs
@@ -11,13 +11,15 @@ module TestHelpers
    setTestThreads,
 
    -- * Test initialization, reading common configs
-   stdTestHarness, stressTest, stressTestReps,
+   stdTestHarness, 
    
    -- * A replacement for defaultMain that uses a 1-thread worker pool 
    defaultMainSeqTests,
 
    -- * Misc utilities
-   nTimes, for_, forDown_, assertOr, timeOut, assertNoTimeOut, splitRange, timeit, 
+   nTimes, for_, forDown_, assertOr, timeOut, assertNoTimeOut, splitRange, timeit,
+   theEnv,
+   
    -- timeOutPure, 
    exceptionOrTimeOut, allowSomeExceptions, assertException
  )
@@ -50,8 +52,6 @@ import Test.Framework.Runners.Options (RunnerOptions'(..))
 import Test.Framework.Options (TestOptions'(..))
 import Test.HUnit as HU
 
--- import Control.LVish.SchedIdempotent (liftIO, dbgLvl, forkWithExceptions)
-import Control.LVish (runParDetailed, Par, OutDest(..))
 import Debug.Trace (trace)
 
 --------------------------------------------------------------------------------
@@ -173,47 +173,6 @@ stdTestHarness genTests = do
                           return all_tests
               _ -> return$ TestList [ setTestThreads n all_tests | n <- all_threads ]
     TF.defaultMain$ hUnitTestToTests tests
-
--- | Run a test repeatedly while using the debugging infrastructure to randomly
--- (artificially) vary thread interleavings.  When a schedule resulting in an
--- incorrect answer (or exception) is found, it is printed.
-stressTest :: Show a =>
-              Word -- ^ Number of repetitions 
-           -> Int  -- ^ Number of workers to run on.  MUST be greater than the maximum
-                   -- number of tasks that can run in parallel; otherwise this will deadlock.
-           -> (forall s . Par d s a)   -- ^ Computation to run
-           -> (a -> Bool) -- ^ Test oracle
-           -> IO ()
-stressTest 0 _workers _comp _oracle = return ()
-stressTest reps workers comp oracle = do 
-  (logs,res) <- runParDetailed (Just(4,10)) [OutputInMemory, OutputEvents] True workers comp
-  let failit s = do threadDelay (500 * 1000)
-                    hPutStrLn stderr $ "\nlstressTest: Found FAILING schedule, length "++show (length logs)
-                    hPutStrLn stderr "-----------------------------------"
-                    mapM_ (hPutStrLn stderr) logs
-                    hPutStrLn stderr "-----------------------------------"
-                    writeFile "failing_sched.log" (unlines logs)
-                    hPutStrLn stderr "Wrote to file: failing_sched.log"
-                    HU.assertFailure s
-  case res of
-    Left exn                 -> failit ("Bad test outcome--exception: "++show exn)
-    Right x | not (oracle x) -> failit ("Bad test result: "++show x)
-            | otherwise -> do putStr "."
-                              stressTest (reps-1) workers comp oracle
-
-defaultNST :: Word
-defaultNST = 100
-
-stressTestReps :: Word
-{-# NOINLINE stressTestReps #-}
-stressTestReps = case lookup "STRESSTESTS" theEnv of
-       Nothing  -> defaultNST
-       Just ""  -> defaultNST
-       Just "0" -> defaultNST
-       Just s   ->
-         case reads s of
-           ((n,_):_) -> trace (" [!] responding to env Var: STRESSTESTS="++show n) n
-           [] -> error$"Attempt to parse STRESSTESTS env var as Int failed: "++show s
 
 
 ----------------------------------------------------------------------------------------------------
