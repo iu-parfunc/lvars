d4e7d0796ed9049fb230bb65bac4d9df6d80f42c
Ryan Newton <rrnewton@gmail.com>
2014-01-23 08:53:00 -0800

    Small tweak to get regression test passing again

--- haskell/regression_test.sh
+++ haskell/regression_test.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# A very simple regression test represinting the minimum standard for each checkin.
+# You may need to run this script with "--reinstall" as an extra argument.
+
 set -x
 set -e
 
 # cabal install -fnewgeneric -fgeneric ./monad-par/monad-par/ ./par-collections/ ./par-classes/ ./par-transformers/ ./lvish/ --enable-tests --force-reinstalls
 
 cabal install -fdebug -fnewgeneric -fgeneric monad-par-0.3.4.6 \
-   ./par-collections/ ./par-classes/ ./par-transformers/ ./lvish/ ./lvish-graph-algorithms/ \
+   ./par-collections/ ./par-classes/ ./par-transformers/ ./lvish/ \
+   ./lvish-apps/pbbs ./lvish-graph-algorithms/ \
    --enable-tests --force-reinstalls -j $*
 
+# TODO, add:
+#   lvish-apps/cfa
