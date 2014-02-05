8bceb820da5f22a72f71dbfb726dff5e16a5f76d
Ryan Newton <rrnewton@gmail.com>
2014-02-04 23:43:03 -0500

    Reenable lvish tests in jenkins.  Small cabal fix.

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -15,8 +15,7 @@ MODE2="--disable-library-profiling --disable-executable-profiling"
 CMDROOT="cabal install $CABAL_FLAGS --reinstall --with-ghc=ghc-$JENKINS_GHC --force-reinstalls"
 
 # TEMP: Disable unit tests for now.
-# --enable-tests
 # There are some serious problems with the testing setup.  
 # Including the atomic-primops related bug on linux, as well as other problems.
 
-$CMDROOT $MODE2 
+$CMDROOT $MODE2 --enable-tests
