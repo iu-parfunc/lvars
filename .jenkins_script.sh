d10e8f15a37a3e5e13909a8cadff610def7677c5
Ryan Newton <rrnewton@gmail.com>
2014-02-04 23:57:42 -0500

    jenkins: need dependencie install before manual configure.

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -10,13 +10,14 @@ cd haskell/lvish
 cabal sandbox init
 cabal sandbox hc-pkg list
 
-if [ "$PROF" == "" || "$PROF" == "0" ]; then 
+if [ "$PROF" == "" ] || [ "$PROF" == "0" ]; then 
   CFG="--disable-library-profiling --disable-executable-profiling"
 else
   CFG="--enable-library-profiling --enable-executable-profiling"
 fi  
 #   --reinstall  --force-reinstalls
 
+cabal install --only-dependencies
 cabal configure $CFG $CABAL_FLAGS --with-ghc=ghc-$JENKINS_GHC
 
 # Avoding the atomic-primops related bug on linux / GHC 7.6:
