41b605b7a9a3946b352e41bbb486b7c1a5de2b0e
Lindsey Kuper <lindsey@composition.al>
2014-02-06 17:02:29 -0500

    Rename MaxCounter -> MaxPosInt; tweak docs a little.
    Also expose `fromMaxPosInt`.

---
+++ haskell/lvish/Data/LVar/MaxPosInt.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE Trustworthy #-}
+
+{-# LANGUAGE DataKinds, BangPatterns, MagicHash #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, MultiParamTypeClasses, TypeFamilies #-} 
+-- | A positive integer LVar that contains the maximum value of all `put`s.
+
+-- TODO: Add 'Min', 'Or', 'And' and other idempotent ops...
+
+module Data.LVar.MaxPosInt
+       ( MaxPosInt,
+         newMaxPosInt, put, waitThresh, freezeMaxPosInt, fromMaxPosInt
+       ) where
+
+import Control.LVish hiding (freeze, put)
+import Control.LVish.Internal (state)
+import Control.LVish.DeepFrz.Internal
+import Data.IORef
+import Data.LVar.Generic
+import Data.LVar.Internal.Pure as P
+import Algebra.Lattice
+import           System.IO.Unsafe  (unsafeDupablePerformIO)
+import           GHC.Prim (unsafeCoerce#)
+
+--------------------------------------------------------------------------------
+
+-- | A @MaxPosInt@ is really a constant-space ongoing @fold max@ operation.
+-- 
+-- A @MaxPosInt@ is an example of a `PureLVar`.  It is implemented simply as a
+-- pure value in a mutable box.
+type MaxPosInt s = PureLVar s MC
+
+newtype MC = MC Int
+  deriving (Eq, Show, Ord, Read)
+
+instance JoinSemiLattice MC where 
+  join (MC !a) (MC !b) = MC (a `max` b)
+
+instance BoundedJoinSemiLattice MC where
+  bottom = MC minBound
+
+-- | Create a new `MaxPosInt` with the given initial value.
+newMaxPosInt :: Int -> Par d s (MaxPosInt s)
+newMaxPosInt n = newPureLVar (MC n)
+
+-- | Incorporate a new value in the max-fold.  If the previous maximum is less than
+-- the new value, increase it.
+put :: MaxPosInt s -> Int -> Par d s ()
+put lv n = putPureLVar lv (MC n)
+
+-- | Wait until the maximum observed value reaches some threshold, then return.
+waitThresh :: MaxPosInt s -> Int -> Par d s ()
+waitThresh lv n = waitPureLVar lv (MC n)
+
+-- | Observe what the final value of the `MaxPosInt` was.
+freezeMaxPosInt :: MaxPosInt s -> Par QuasiDet s Int
+freezeMaxPosInt lv = do
+  MC n <- freezePureLVar lv
+  return n
+
+-- | Once frozen, for example by `runParThenFreeze`, a `MaxPosInt` can be converted
+-- directly into an `Int`.
+fromMaxPosInt :: MaxPosInt Frzn -> Int
+fromMaxPosInt (PureLVar lv) =
+  case unsafeDupablePerformIO (readIORef (state lv)) of
+    MC n -> n
+
+instance DeepFrz MC where
+   type FrzType MC = MC
