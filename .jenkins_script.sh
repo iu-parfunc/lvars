85a451d38793c2cd9e3c7d46a89d282f7adc0cc8
Ryan Newton <rrnewton@gmail.com>
2014-05-01 01:58:51 -0400

    Back off of cabal 1.20 to cabal 1.18

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -6,6 +6,11 @@
 set -e
 set -x
 
+# Temporarily staying off of 1.20 due to cabal issue #1811:
+CABAL=cabal-1.18.0
+SHOWDETAILS=always
+# SHOWDETAILS=streaming
+
 if [ "$JENKINS_GHC" == "" ]; then 
   GHC=ghc
 else
@@ -21,11 +26,11 @@ PKGS=" ./lvish ./par-classes ./par-collections ./par-transformers"
 
 cd ./haskell/
 TOP=`pwd`
-cabal sandbox init
-cabal sandbox hc-pkg list
+$CABAL sandbox init
+$CABAL sandbox hc-pkg list
 for path in $PKGS; do 
   cd $TOP/$path
-  cabal sandbox init --sandbox=$TOP/.cabal-sandbox
+  $CABAL sandbox init --sandbox=$TOP/.cabal-sandbox
 done
 cd $TOP
 
@@ -37,11 +42,11 @@ fi
 #   --reinstall  --force-reinstalls
 
 # Temporary hack, install containers first or we run into problems with the sandbox:
-cabal install containers --constraint='containers>=0.5.5.1'
+# $CABAL install containers --constraint='containers>=0.5.5.1'
 
 # Also install custom version of monad-par:
-cabal install $CFG $CABAL_FLAGS --with-ghc=$GHC $PKGS ./monad-par/monad-par/ $*
-cabal install $CFG $CABAL_FLAGS --with-ghc=$GHC $PKGS ./monad-par/monad-par/ --enable-tests --only-dep $*
+$CABAL install $CFG $CABAL_FLAGS --with-ghc=$GHC $PKGS ./monad-par/monad-par/ --enable-tests --only-dep $*
+$CABAL install $CFG $CABAL_FLAGS --with-ghc=$GHC $PKGS ./monad-par/monad-par/ $*
 
 # Avoding the atomic-primops related bug on linux / GHC 7.6:
 if ! [ `uname` == "Linux" ]; then  
@@ -49,6 +54,6 @@ if ! [ `uname` == "Linux" ]; then
     echo "Test package in path $path."
     cd $TOP/$path
     # Assume cabal 1.20+:
-    cabal test --show-details=streaming
+    cabal test --show-details=$SHOWDETAILS
   done
 fi
