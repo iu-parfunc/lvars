68c74f191b341dfc44b48519665cd1f984dee21a
Ryan Newton <rrnewton@gmail.com>
2014-04-30 22:17:47 -0400

    Minor: change jenkins package install order.

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -37,8 +37,8 @@ fi
 #   --reinstall  --force-reinstalls
 
 # Install custom version of monad-par:
-cabal install $CFG $CABAL_FLAGS --with-ghc=$GHC $PKGS --only-dep --enable-tests ./monad-par/monad-par/ $*
-cabal install $CFG $CABAL_FLAGS --with-ghc=$GHC $PKGS $*
+cabal install $CFG $CABAL_FLAGS --with-ghc=$GHC $PKGS ./monad-par/monad-par/ $*
+cabal install $CFG $CABAL_FLAGS --with-ghc=$GHC $PKGS ./monad-par/monad-par/ --enable-tests --only-dep $*
 
 # Avoding the atomic-primops related bug on linux / GHC 7.6:
 if ! [ `uname` == "Linux" ]; then  
