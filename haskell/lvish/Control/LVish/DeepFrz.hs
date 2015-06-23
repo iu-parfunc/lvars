826553974976af30fe2f29d58be1adfab65388ca
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-06-23 09:32:01 -0400

    move some orphan instances to the TC definition site

--- haskell/lvish/Control/LVish/DeepFrz.hs
+++ haskell/lvish/Control/LVish/DeepFrz.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE Trustworthy #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE ConstraintKinds   #-}
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE MagicHash         #-}
+{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE Trustworthy       #-}
+{-# LANGUAGE TypeFamilies      #-}
 
 {-|
 
@@ -21,12 +20,12 @@ example:
 
 > {-# LANGUAGE TypeFamilies #-}
 > import Control.LVish.DeepFrz
-> 
+>
 > data MyData = MyData Int deriving Show
-> 
+>
 > instance DeepFrz MyData where
 >   type FrzType MyData = MyData
-> 
+>
 > main = print (runParThenFreeze (return (MyData 3)))
 
 -}
@@ -44,16 +43,13 @@ module Control.LVish.DeepFrz
          -- * Some supporting types
          DeepFrz(), FrzType,
          NonFrzn, Frzn, Trvrsbl,
-         
+
        ) where
 
-import Data.Int
-import Data.Word
-import GHC.Prim (unsafeCoerce#)
-
 -- import Control.LVish (LVarData1(..))
-import Control.LVish.DeepFrz.Internal (DeepFrz(..), NonFrzn, Frzn, Trvrsbl)
-import Control.LVish.Internal (Par(WrapPar))
+import Control.LVish.DeepFrz.Internal         (DeepFrz (..), Frzn, NonFrzn,
+                                               Trvrsbl)
+import Control.LVish.Internal                 (Par (WrapPar))
 import Control.Par.EffectSigs
 import Internal.Control.LVish.SchedIdempotent (runPar, runParIO)
 --------------------------------------------------------------------------------
@@ -65,7 +61,7 @@ import Internal.Control.LVish.SchedIdempotent (runPar, runParIO)
 -- all data races, and freezing is therefore safe.  Running a `Par`
 -- computation with `runParThenFreeze` accomplishes this, without our
 -- having to call `freeze` explicitly.
--- 
+--
 -- In order to use `runParThenFreeze`, the type returned from the
 -- `Par` computation must be a member of the `DeepFrz` class.  All the
 -- @Data.LVar.*@ libraries should provide instances of `DeepFrz`
@@ -73,7 +69,7 @@ import Internal.Control.LVish.SchedIdempotent (runPar, runParIO)
 -- pure datatypes.  The result of a `runParThenFreeze` depends on the
 -- type-level function `FrzType`, whose only purpose is to toggle the
 -- `s` parameters of all IVars to the `Frzn` state.
--- 
+--
 -- Significantly, the freeze at the end of `runParThenFreeze` has /no/ runtime cost, in
 -- spite of the fact that it enables a /deep/ (recursive) freeze of the value returned
 -- by the `Par` computation.
@@ -82,7 +78,7 @@ runParThenFreeze :: DeepFrz a => Par (Ef P G NF B NI) NonFrzn a -> FrzType a
 runParThenFreeze (WrapPar p) = frz $ runPar p
 
 -- | This version works for nondeterministic computations as well.
--- 
+--
 -- Of course, nondeterministic computations may also call `freeze`
 -- internally, but this function has an advantage to doing your own
 -- `freeze` at the end of a `runParIO`: there is an implicit barrier
@@ -99,78 +95,3 @@ instance (LVarData1 f, DeepFrz a) => DeepFrz (f s a) where
   type FrzType (f s a) = f Frzn (FrzType a)
   frz = unsafeCoerce#
 -}
-
-#define MKFRZINST(T) instance DeepFrz T where type FrzType T = T
-
-MKFRZINST(Int)
-MKFRZINST(Int8)
-MKFRZINST(Int16)
-MKFRZINST(Int32)
-MKFRZINST(Int64)
-MKFRZINST(Word)
-MKFRZINST(Word8)
-MKFRZINST(Word16)
-MKFRZINST(Word32)
-MKFRZINST(Word64)
-MKFRZINST(Bool)
-MKFRZINST(Char)
-MKFRZINST(Integer)
-MKFRZINST(Float)
-MKFRZINST(Double)
-
-MKFRZINST(())
-MKFRZINST(Ordering)
-
-instance DeepFrz a => DeepFrz [a] where
-  type FrzType [a] = [FrzType a]
-  frz = unsafeCoerce#
-
-instance DeepFrz a => DeepFrz (Maybe a) where
-  type FrzType (Maybe a) = Maybe (FrzType a)
-  frz = unsafeCoerce#
-
-instance (DeepFrz a, DeepFrz b) => DeepFrz (Either a b) where
-  type FrzType (Either a b) = Either (FrzType a) (FrzType b)
-  frz = unsafeCoerce#
-
-instance (DeepFrz a, DeepFrz b) => DeepFrz (a,b) where
-  type FrzType (a,b) = (FrzType a,FrzType b)
-  frz = unsafeCoerce#
-
-instance (DeepFrz a, DeepFrz b, DeepFrz c) => DeepFrz (a,b,c) where
-  type FrzType (a,b,c) = (FrzType a,FrzType b,FrzType c)
-  frz = unsafeCoerce#
-
-instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d) => DeepFrz (a,b,c,d) where
-  type FrzType (a,b,c,d) = (FrzType a, FrzType b, FrzType c, FrzType d)
-  frz = unsafeCoerce#
-
-instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d, DeepFrz e) => DeepFrz (a,b,c,d,e) where
-  type FrzType (a,b,c,d,e) = (FrzType a, FrzType b, FrzType c, FrzType d, FrzType e)
-  frz = unsafeCoerce#
-
-instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d, DeepFrz e,
-          DeepFrz f) => DeepFrz (a,b,c,d,e,f) where
-  type FrzType (a,b,c,d,e,f) = (FrzType a, FrzType b, FrzType c, FrzType d, FrzType e,
-                                FrzType f)
-  frz = unsafeCoerce#
-
-instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d, DeepFrz e,
-          DeepFrz f, DeepFrz g) => DeepFrz (a,b,c,d,e,f,g) where
-  type FrzType (a,b,c,d,e,f,g) = (FrzType a, FrzType b, FrzType c, FrzType d, FrzType e,
-                                  FrzType f, FrzType g)
-  frz = unsafeCoerce#
-
-
-instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d, DeepFrz e,
-          DeepFrz f, DeepFrz g, DeepFrz h) => DeepFrz (a,b,c,d,e,f,g,h) where
-  type FrzType (a,b,c,d,e,f,g,h) = (FrzType a, FrzType b, FrzType c, FrzType d, FrzType e,
-                                    FrzType f, FrzType g, FrzType h)
-  frz = unsafeCoerce#
-
-
-instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d, DeepFrz e,
-          DeepFrz f, DeepFrz g, DeepFrz h, DeepFrz i) => DeepFrz (a,b,c,d,e,f,g,h,i) where
-  type FrzType (a,b,c,d,e,f,g,h,i) = (FrzType a, FrzType b, FrzType c, FrzType d, FrzType e,
-                                      FrzType f, FrzType g, FrzType h, FrzType i)
-  frz = unsafeCoerce#
--- haskell/lvish/Control/LVish/DeepFrz/Internal.hs
+++ haskell/lvish/Control/LVish/DeepFrz/Internal.hs
@@ -1,15 +1,21 @@
-{-# LANGUAGE Unsafe #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE CPP               #-}
 {-# LANGUAGE DefaultSignatures #-}
-{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE EmptyDataDecls    #-}
+{-# LANGUAGE MagicHash         #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE Unsafe            #-}
 
 -- | This module is /not/ Safe Haskell, but it must be used to create
 -- new LVar types.
 module Control.LVish.DeepFrz.Internal
        (
-         DeepFrz(..), NonFrzn, Frzn, Trvrsbl 
+         DeepFrz(..), NonFrzn, Frzn, Trvrsbl
        )
        where
+
+import Data.Int
+import Data.Word
+import GHC.Prim  (unsafeCoerce#)
 
 -- | DeepFreezing is a type-level (guaranteed /O(1)/ time complexity)
 -- operation.  It marks an LVar and its contents (recursively) as
@@ -29,8 +35,8 @@ class DeepFrz a where
   -- | While `frz` is not exported, users may opt-in to the `DeepFrz`
   -- class for their datatypes and take advantage of the default instance.
   -- Doing so REQUIRES that `type FrzType a = a`.
-  default frz :: a -> a 
-  frz a = a 
+  default frz :: a -> a
+  frz a = a
 
 -- | An uninhabited type that signals that an LVar has been frozen.
 -- LVars should use this in place of their `s` parameter.
@@ -43,5 +49,81 @@ data NonFrzn
 -- | An uninhabited type that signals that an LVar is not only frozen,
 -- but it may be traversed in whatever order its internal
 -- representation dictates.
-data Trvrsbl 
+data Trvrsbl
 
+--------------------------------------------------------------------------------
+
+#define MKFRZINST(T) instance DeepFrz T where type FrzType T = T
+
+MKFRZINST(Int)
+MKFRZINST(Int8)
+MKFRZINST(Int16)
+MKFRZINST(Int32)
+MKFRZINST(Int64)
+MKFRZINST(Word)
+MKFRZINST(Word8)
+MKFRZINST(Word16)
+MKFRZINST(Word32)
+MKFRZINST(Word64)
+MKFRZINST(Bool)
+MKFRZINST(Char)
+MKFRZINST(Integer)
+MKFRZINST(Float)
+MKFRZINST(Double)
+
+MKFRZINST(())
+MKFRZINST(Ordering)
+
+instance DeepFrz a => DeepFrz [a] where
+  type FrzType [a] = [FrzType a]
+  frz = unsafeCoerce#
+
+instance DeepFrz a => DeepFrz (Maybe a) where
+  type FrzType (Maybe a) = Maybe (FrzType a)
+  frz = unsafeCoerce#
+
+instance (DeepFrz a, DeepFrz b) => DeepFrz (Either a b) where
+  type FrzType (Either a b) = Either (FrzType a) (FrzType b)
+  frz = unsafeCoerce#
+
+instance (DeepFrz a, DeepFrz b) => DeepFrz (a,b) where
+  type FrzType (a,b) = (FrzType a,FrzType b)
+  frz = unsafeCoerce#
+
+instance (DeepFrz a, DeepFrz b, DeepFrz c) => DeepFrz (a,b,c) where
+  type FrzType (a,b,c) = (FrzType a,FrzType b,FrzType c)
+  frz = unsafeCoerce#
+
+instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d) => DeepFrz (a,b,c,d) where
+  type FrzType (a,b,c,d) = (FrzType a, FrzType b, FrzType c, FrzType d)
+  frz = unsafeCoerce#
+
+instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d, DeepFrz e) => DeepFrz (a,b,c,d,e) where
+  type FrzType (a,b,c,d,e) = (FrzType a, FrzType b, FrzType c, FrzType d, FrzType e)
+  frz = unsafeCoerce#
+
+instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d, DeepFrz e,
+          DeepFrz f) => DeepFrz (a,b,c,d,e,f) where
+  type FrzType (a,b,c,d,e,f) = (FrzType a, FrzType b, FrzType c, FrzType d, FrzType e,
+                                FrzType f)
+  frz = unsafeCoerce#
+
+instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d, DeepFrz e,
+          DeepFrz f, DeepFrz g) => DeepFrz (a,b,c,d,e,f,g) where
+  type FrzType (a,b,c,d,e,f,g) = (FrzType a, FrzType b, FrzType c, FrzType d, FrzType e,
+                                  FrzType f, FrzType g)
+  frz = unsafeCoerce#
+
+
+instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d, DeepFrz e,
+          DeepFrz f, DeepFrz g, DeepFrz h) => DeepFrz (a,b,c,d,e,f,g,h) where
+  type FrzType (a,b,c,d,e,f,g,h) = (FrzType a, FrzType b, FrzType c, FrzType d, FrzType e,
+                                    FrzType f, FrzType g, FrzType h)
+  frz = unsafeCoerce#
+
+
+instance (DeepFrz a, DeepFrz b, DeepFrz c, DeepFrz d, DeepFrz e,
+          DeepFrz f, DeepFrz g, DeepFrz h, DeepFrz i) => DeepFrz (a,b,c,d,e,f,g,h,i) where
+  type FrzType (a,b,c,d,e,f,g,h,i) = (FrzType a, FrzType b, FrzType c, FrzType d, FrzType e,
+                                      FrzType f, FrzType g, FrzType h, FrzType i)
+  frz = unsafeCoerce#
