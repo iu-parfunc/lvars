306c14400f0ec8c9bf3cd9cc57b2f19df2f178be
Ryan Newton <rrnewton@gmail.com>
2015-07-27 17:32:24 -0400

    par-mergesort: Documentation and placeholders

--- haskell/par-mergesort/Control/Par/MergeSort.hs
+++ haskell/par-mergesort/Control/Par/MergeSort.hs
@@ -1,12 +1,36 @@
--- | Expose a fast, parallel mergesort.
+-- | A fast, parallel mergesort.
 --
 --   This module exposes good default configurations, with simple interfaces.
 
 module Control.Par.MergeSort
-       ( sortPar
+       (
+       -- * Simple sorts
+
+       -- | These are drop-in replacements for their counterparts in `vector-algorithms`
+       sort, sortBy, Comparison,
+
+       -- * Par-monad sorts
+       sortPar
+
+       -- * Unboxed and storable sorts
+
        )
  where
 
+import Control.Monad.Primitive
+import Data.Vector.Generic.Mutable
 
+type Comparison e = e -> e -> Ordering
+
+--------------------------------------------------------------------------------
+
+sort :: (PrimMonad m, MVector v e, Ord e) => v (PrimState m) e -> m ()
+sort = undefined
+
+sortBy :: (PrimMonad m, MVector v e) => Comparison e -> v (PrimState m) e -> m ()
+sortBy = undefined
+
+
+--------------------------------------------------------------------------------
 
 sortPar = undefined
