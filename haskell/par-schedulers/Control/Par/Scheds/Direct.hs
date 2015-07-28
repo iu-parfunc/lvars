d8e9854deab3855ca0bb2f02f4a3feb22dbb43e7
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-07-28 17:27:13 -0400

    started implementing par-schedulers

---
+++ haskell/par-schedulers/Control/Par/Scheds/Direct.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilies   #-}
+
+module Control.Par.Scheds.Direct where
+
+import           System.IO.Unsafe                (unsafePerformIO)
+
+import           Control.Par.Class
+import qualified Control.Par.Class.Unsafe        as PC
+import           Control.Par.EffectSigs
+
+import           Control.Monad.Par.Scheds.Direct as D
+
+newtype Direct (e :: EffectSig) s a =
+  Direct { unwrapDirect :: Par a }
+
+newtype DirectIVar s a = DirectIVar { unwrapDirectIVar :: D.IVar a }
+
+instance PC.ParMonad Direct where
+  pbind (Direct p1) p2 = Direct $ p1 >>= unwrapDirect . p2
+  preturn = Direct . return
+  fork = Direct . D.fork . unwrapDirect
+  internalLiftIO = return . unsafePerformIO
+
+instance ParFuture Direct where
+  type Future Direct = DirectIVar
+  type FutContents Direct a = ()
+  spawn_ = Direct . fmap DirectIVar . D.spawn_ . unwrapDirect
+  get = Direct . D.get . unwrapDirectIVar
---
+++ haskell/par-schedulers/Control/Par/Scheds/Sparks.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilies   #-}
+
+module Control.Par.Scheds.Sparks where
+
+import           Control.Monad                   (void)
+import           System.IO.Unsafe                (unsafePerformIO)
+
+import           Control.Par.Class
+import qualified Control.Par.Class.Unsafe        as PC
+import           Control.Par.EffectSigs
+
+import qualified Control.Monad.Par.Scheds.Sparks as S
+
+newtype Sparks (e :: EffectSig) s a =
+  Sparks { unwrapSparks :: S.Par a }
+
+newtype SparksFuture s a = SparksFuture { unwrapSparksFuture :: S.Future a }
+
+instance PC.ParMonad Sparks where
+  pbind (Sparks p1) p2 = Sparks $ p1 >>= unwrapSparks . p2
+  preturn = Sparks . return
+  fork = void . Sparks . S.spawn_ . unwrapSparks
+  internalLiftIO = return . unsafePerformIO
+
+instance ParFuture Sparks where
+  type Future Sparks = SparksFuture
+  type FutContents Sparks a = ()
+  spawn_ = Sparks . fmap SparksFuture . S.spawn_ . unwrapSparks
+  get = Sparks . S.get . unwrapSparksFuture
