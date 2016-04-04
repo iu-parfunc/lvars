4e0eccae4bb82f74bec30cf9577efab8123cfb66
Ryan Newton <rrnewton@gmail.com>
2016-04-04 14:32:36 -0400

    Try a new approach for travis config.  Docker+cached dirs.

---
+++ .travis_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Assumes that "stack" is in the path:
+
+set -xe
+
+cat stack.yaml | grep -v resolver > stack-${STACK_RESOLVER}.yaml
+echo "resolver: ${STACK_RESOLVER}" >> stack-${STACK_RESOLVER}.yaml
+rm -f stack.yaml # Just to make sure.
+
+# In this mode we just grab the latest from hackage:
+if [ ${STACK_RESOLVER%-*} = "ghc" ]; then
+    which -a ghc
+    ghc --version
+    stack --resolver=${STACK_RESOLVER} solver --modify-stack-yaml
+fi
+
+# Sweet and simple.  Install upstream dependencies, including GHC:
+stack setup --no-terminal
+stack test --only-snapshot --no-terminal
