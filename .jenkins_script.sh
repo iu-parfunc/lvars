256cf7dde2f4bb1da7d43b1e16694b1a25ba1150
Ryan Newton <rrnewton@gmail.com>
2016-04-04 14:59:15 -0400

    Modify jenkins script to run tests through stack.

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -3,7 +3,7 @@
 # NOTE: Passes through extra args to the major cabal install command.
 #       Also uses these environment vars, if available:
 #        * JENKINS_GHC
-#        * CABAL_FLAGS[1-3]
+#        * STACK_FLAGS
 #        * NOTEST
 #        * CABAL
 #        * EXTRAPKGS -- useful for including packages in the one-big-install
@@ -13,45 +13,18 @@ set -x
 
 SHOWDETAILS=streaming
 
-if [ "$JENKINS_GHC" == "" ]; then
-  GHC=ghc
-else
-  ENVSCRIPT=$HOME/rn_jenkins_scripts/acquire_ghc.sh
-  # This is specific to our testing setup at IU:
-  if [ -f "$ENVSCRIPT" ]; then
-    source "$ENVSCRIPT"
-  fi
-  GHC=ghc-$JENKINS_GHC
-fi
+# Just use which stack is in scope:
+STACK=stack
+# STACK=stack-1.0.4.2
 
-# Now requiring a recent version of cabal:
-if [ "$CABAL" == "" ]; then
-  if [ "$GHC" == "ghc-7.10.1" ]; then
-      CABAL=cabal-1.22
-      DISABLE_EXEC_PROF="--disable-profiling"
-      ENABLE_EXEC_PROF="--enable-profiling"
-  else
-      CABAL=cabal-1.20
-      DISABLE_EXEC_PROF="--disable-executable-profiling"
-      ENABLE_EXEC_PROF="--enable-executable-profiling"
-  fi
-fi
-
-PKGS=" ./lvish ./par-classes ./par-collections ./par-transformers ./concurrent-skiplist ./par-mergesort "
-
-# We build the sandbox, not at the repo root, but at the root of the Haskell code dir.
-cd ./haskell/
-TOP=`pwd`
-$CABAL sandbox init
-$CABAL sandbox hc-pkg list
-for path in $PKGS; do
-  cd $TOP/$path
-  $CABAL sandbox init --sandbox=$TOP/.cabal-sandbox
-done
-cd $TOP
+DISABLE_EXEC_PROF="--disable-profiling"
+ENABLE_EXEC_PROF="--enable-profiling"
+# Old version, GHC-7.8:
+# DISABLE_EXEC_PROF="--disable-executable-profiling"
+# ENABLE_EXEC_PROF="--enable-executable-profiling"
 
 # Always make sure the benchmarks build, even if we don't run them:
-CFG=" --force-reinstalls --enable-benchmarks "
+CFG=" --bench $STACK_FLAGS "
 
 if [ "$PROF" == "" ] || [ "$PROF" == "0" ]; then
   CFG="$CFG --disable-library-profiling $DISABLE_EXEC_PROF"
@@ -63,18 +36,4 @@ if [ "$NOTEST" == "" ]; then
   CFG="$CFG --enable-tests"
 fi
 
-# In newer cabal (>= 1.20) --enable-tests is separate from --run-tests:
-$CABAL install $CFG $CABAL_FLAGS --with-ghc=$GHC -fdebug $PKGS $EXTRAPKGS $*
-
-if [ "$NOTEST" == "" ]; then
-  for path in $PKGS; do
-    echo "Test package in path $path."
-    cd $TOP/$path
-    # Assume cabal 1.20+:
-    echo "Do a reconfigure to make sure test doesn't rebuild with different arguments."
-    $CABAL configure --with-ghc=$GHC -fdebug --enable-tests $CABAL_FLAGS
-    $CABAL test --show-details=$SHOWDETAILS --test-options='-j1'
-# FIXME: we lost xml output when switching to tasty.  Restore.
-#    --jxml=test-results.xml --jxml-nested
-  done
-fi
+stack --no-system-ghc --install-ghc test $CFG
