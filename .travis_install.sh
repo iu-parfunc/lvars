6e6243144681b81a55227c69ca386410d18b8bda
Chao-Hong Chen <dreamlinuxer@gmail.com>
2016-08-03 18:55:23 -0400

    update travis script

--- .travis_install.sh
+++ .travis_install.sh
@@ -4,15 +4,15 @@
 
 set -xe
 
-cat stack.yaml | grep -v resolver > stack-${STACK_RESOLVER}.yaml
-echo "resolver: ${STACK_RESOLVER}" >> stack-${STACK_RESOLVER}.yaml
-rm -f stack.yaml # Just to make sure.
-
 # In this mode we just grab the latest from hackage:
 if [ ${STACK_RESOLVER%-*} = "default" ]; then
     which -a ghc
     ghc --version
-    stack --resolver=${STACK_RESOLVER} solver --modify-stack-yaml
+    #    stack --resolver=${STACK_RESOLVER} solver --modify-stack-yaml
+else
+    cat stack.yaml | grep -v resolver > stack-${STACK_RESOLVER}.yaml
+    echo "resolver: ${STACK_RESOLVER}" >> stack-${STACK_RESOLVER}.yaml
+    rm -f stack.yaml # Just to make sure.
 fi
 
 # Sweet and simple.  Install upstream dependencies, including GHC:
